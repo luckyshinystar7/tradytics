4c5410ad1e4ab181ea418fbd168d113a28d123dd
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-08 20:45:33 -0300

    finaliza ex06 manual sobre movimento de pecas de xadrez

--- 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex06-manual-xadrez.js
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex06-manual-xadrez.js
@@ -4,3 +4,40 @@
 // Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 // Exemplo: bishop (bispo) -> diagonals (diagonais)
 
+const pawn = "forward 1 square";
+const bishop = "diagonal any distance";
+const knight = "L";
+const rook = "forward and sideways any distance";
+const queen = "anyway and any distance";
+const king = "anyway 1 square";
+
+let piece = "KING".toLowerCase();
+
+switch (piece) {
+  case "pawn":
+    console.log(pawn);
+    break;
+
+  case "bishop":
+    console.log(bishop);
+    break;
+
+  case "knight":
+    console.log(knight);
+    break;
+
+  case "rook":
+    console.log(rook);
+    break;
+
+  case "queen":
+    console.log(queen);
+    break;
+
+  case "king":
+    console.log(king);
+    break;
+
+  default:
+    console.log("Your piece invalid. Try again using another one.");
+}
