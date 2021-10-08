c810c7705eeda8f02d047d4836254868a6c82a31
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-08 18:10:35 -0300

    primeiro ex de fixacao

---
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/fix01.js
@@ -0,0 +1,6 @@
+const myName = "Elthon";
+const birthCity = "São Bernardo do Campo";
+let birthYear = 1989;
+// myName = "peroba"; Não é possível alterar o valor de constantes.
+
+console.log("Meu nome é",myName,"eu nasci em", birthCity, "no ano de",birthYear, ".");
