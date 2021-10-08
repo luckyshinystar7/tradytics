81563a6296089dcede942a096467b61cc9ec4350
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-08 20:30:28 -0300

    adiciona enunciado nos exercicios e inicia ex06 manual pecas de xadrez

--- 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex05-angulos-triangulo.js
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex05-angulos-triangulo.js
@@ -1,3 +1,7 @@
+// Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+// Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+// Um ângulo será considerado inválido se não tiver um valor positivo.
+
 const a = 120;
 const b = 10;
 const c = 50;
