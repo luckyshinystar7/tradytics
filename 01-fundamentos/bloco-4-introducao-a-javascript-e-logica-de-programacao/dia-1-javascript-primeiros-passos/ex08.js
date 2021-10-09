ab806878a171151bee40ac3adfdc8e46dd6a07a0
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-09 16:28:27 -0300

    finaliza ex 8

---
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex08.js
@@ -0,0 +1,11 @@
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+const c1 = 11;
+const c2 = 7;
+const c3 = 14;
+
+if (c1 % 2 == 0 || c2 % 2 == 0 || c3 % 2 == 0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
