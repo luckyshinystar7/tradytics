65e09117d481ac3967648153f16752af8af19215
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-14 15:40:59 -0300

    finaliza e debuga ex01

--- 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex01-quadrado-de-asteriscos.js
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex01-quadrado-de-asteriscos.js
@@ -1,1 +1,11 @@
 // Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
+const n = 5;
+var linha = "";
+
+for (let i = 0; i < n; i++) {
+  linha += "*";
+}
+
+for (let i = 0; i < n; i++) {
+  console.log(linha);
+}
