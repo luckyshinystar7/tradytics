82c4368a744c4566f287c8a1a8802dd47ce56878
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-15 19:01:00 -0300

    finaliza exercicio de fixacao 2 parte 2

--- 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fix02-forin.js
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fix02-forin.js
@@ -1,4 +1,4 @@
-// Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+// 1- Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
 let names = {
   person1: 'João',
   person2: 'Maria',
@@ -6,4 +6,14 @@ let names = {
 };
 for (let key in names) {
   console.log('Olá '+ names[key]);
+}
+
+// 2- Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+for (let key in car) {
+  console.log(key,': ',car[key]);
 }
