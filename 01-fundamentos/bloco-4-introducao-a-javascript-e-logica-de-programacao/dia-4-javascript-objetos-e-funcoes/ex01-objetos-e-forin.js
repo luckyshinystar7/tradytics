3c365fa10193322d6c9b27443b89632fa5ef911e
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-16 12:13:59 -0300

    finaliza parte 2

--- 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex01-objetos-e-forin.js
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex01-objetos-e-forin.js
@@ -7,4 +7,10 @@ let info = {
 
 //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console: Bem-vinda, Margarida
 
-console.log('Bem-vinda, ' + info.personagem);
+//console.log('Bem-vinda, ' + info.personagem);
+
+//2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+info['recorrente'] = 'Sim';
+console.log(info);
+
+info.recorrente = 'Sim'; // Dúvida: Posso criar uma key com um valor dentro usando notação ponto? Quando crio funciona normalmente porém, o VSCode me da uma mensagem de alerta indicando que a propriedade não existe. ts(2339)
