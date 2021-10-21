7accf4aeab2ddce7b925239834594b26007d35ee
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-21 19:28:33 -0300

    finaliza exercicios de fixacao parte I

--- 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fix02.js
+++ 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fix02.js
@@ -1,9 +1,26 @@
 // 1- Acesse o elemento elementoOndeVoceEsta .
-let req01 = document.getElementById('elementoOndeVoceEsta');
+let ondeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 
 // 2- Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-req01.parentElement.style.color = 'red';
+let pai = req01.parentElement;
+pai.style.color = 'blue';
 
 //  3- Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
-let filhoDoFilho = document.querySelector('#filhoDoFilho');
-filhoDoFilho.innerHTML('Eu sou o neto');
+let primeiroFilhoDoFilho = document.getElementById("primeiroFilhoDoFilho");
+primeiroFilhoDoFilho.innerText = 'Será que esse texto vai entrar? Entrou!';
+
+// 4- Acesse o primeiroFilho a partir de pai .
+pai.firstElementChild;
+
+// 5- Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+ondeVoceEsta.previousElementSibling;
+
+// 6- Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+ondeVoceEsta.parentElement.innerText;
+//ou
+ondeVoceEsta.nextSibling;
+// 7- Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+ondeVoceEsta.nextElementSibling;
+
+// 8- Agora acesse o terceiroFilho a partir de pai .
+pai.lastElementChild.previousElementSibling;
