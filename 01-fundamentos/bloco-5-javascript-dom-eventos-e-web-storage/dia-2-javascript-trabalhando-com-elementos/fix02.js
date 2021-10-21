bb11f505da63061787e85abf5b1b38950854e545
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-21 15:42:36 -0300

    continua fix2

--- 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fix02.js
+++ 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fix02.js
@@ -2,4 +2,8 @@
 let req01 = document.getElementById('elementoOndeVoceEsta');
 
 // 2- Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-req01.parentElement.style.backgroundColor = 'red';  
+req01.parentElement.style.color = 'red';
+
+//  3- Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+let filhoDoFilho = document.querySelector('#filhoDoFilho');
+filhoDoFilho.innerHTML('Eu sou o neto');
