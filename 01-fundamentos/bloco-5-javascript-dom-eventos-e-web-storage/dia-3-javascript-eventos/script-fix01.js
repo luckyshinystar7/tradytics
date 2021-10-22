dca9432b3d273c969ed8419f943c9d1786912d62
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-22 10:20:35 -0300

    cria funcao que ao digitar na caixa de texto, altera o elemento com a classe tech

--- 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script-fix01.js
+++ 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script-fix01.js
@@ -34,11 +34,15 @@ thirdLi.addEventListener('click', addTechThird);
 
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 // Implementa uma função que remove a classe dos outros ao adicionar no selecionado.
-
+// feito!
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('keyup', function() {
+  let techElement = document.querySelector('.tech');
 
+  techElement.innerText = 'Texto alterado';
+})
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
