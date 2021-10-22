767976482b3e9410506316960bb161b74ef7a89c
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-22 12:04:05 -0300

    finaliza req 01

---
+++ 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,36 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+//Exercicio 1
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const ul = document.querySelector('ul');
+for (let i = 0 ; i < dezDaysList.length ; i += 1){
+  let day = dezDaysList[i];
+  let dayItem = document.createElement('li');
+  dayItem.innerText = day;
+  dayItem.className = 'day';
+  if (day == 24 || day == 25 || day == 31) {
+    dayItem.className += ' holiday';
+  }
+  if ( day == 4 || day == 11 || day == 18 || day == 25 ) {
+    dayItem.className += ' friday';
+  }
+
+  ul.appendChild(dayItem);
+
+};
+
