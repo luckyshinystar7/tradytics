e885610853998842787d40dd8c9cc285eac15337
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-10-28 19:34:37 -0300

    botao que imprime informacoes ou alerta que falta informacao obrigatoria criado com sucesso

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -27,10 +27,29 @@ const brStates = [
   "SE",
   "TO",
 ];
-const stateSelect = document.querySelector('#state');
+const stateSelect = document.querySelector("#state");
 
-for (let i = 0 ; i < brStates.length ; i += 1) {
-  let opt = document.createElement('option');
+for (let i = 0; i < brStates.length; i += 1) {
+  let opt = document.createElement("option");
   opt.text = brStates[i];
   stateSelect.appendChild(opt);
 }
+const btn = document.querySelector("#button");
+const printElement = document.querySelector('#print');
+const inputList = document.querySelectorAll(".required");
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let content = "";
+  let print;
+  for (let i = 0; i < inputList.length; i += 1) {
+    if (inputList[i].value == "") {
+      print = `${inputList[i].id} é um requisito obrigatório`;
+      break;
+    } else {
+      content = content + ", " + inputList[i].value;
+      print = content;
+    }
+  }
+  printElement.innerText = print;
+});
