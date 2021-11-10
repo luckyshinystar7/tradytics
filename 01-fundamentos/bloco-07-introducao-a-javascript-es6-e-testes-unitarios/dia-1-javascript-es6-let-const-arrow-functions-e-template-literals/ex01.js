b3696908bccff249228e0b8c1eea4b584e78283a
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-10 19:24:59 -0300

    finaliza ex1 e insere enunciado completo dentro do arquivo JS

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ex01.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ex01.js
@@ -2,19 +2,19 @@
 // Modifique a estrutura da função para que ela seja uma arrow function .
 // Modifique as concatenações para template literals .
 
-// const b = (escopo) => {
+// function testingScope(escopo) {
 //   if (escopo === true) {
-//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
 //     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
 //     console.log(ifScope);
 //   } else {
-//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
 //     console.log(elseScope);
 //   }
 //   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 // }
 
-// console.log(b);
+// testingScope(true);
 
 const b = (escopo) => {
   if (escopo === true) {
