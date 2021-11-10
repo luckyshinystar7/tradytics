7cd6ca86e3b9dbc097a8e08810500e2b39d50045
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-10 19:18:20 -0300

    corrige os vazamentos de escopo trocando var por let

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ex01.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ex01.js
@@ -1,10 +1,28 @@
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function .
+// Modifique as concatenações para template literals .
+
+// const b = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+// console.log(b);
+
 function testingScope(escopo) {
   if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
     console.log(ifScope);
   } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
