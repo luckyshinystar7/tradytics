20d58d480abf7e7896f848b937d88e46a3927fe1
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-13 11:10:47 -0300

    finaliza ex 2.8

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/Exercicios-Parte2.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/Exercicios-Parte2.js
@@ -72,7 +72,7 @@ const totalStudents = (obj) => {
   return somaStudents;
 }
 
-console.log('Resposta Ex06');
+console.log('Resposta Ex06 expected: /n 50');
 console.log(totalStudents(allLessons));
 
 // 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
@@ -81,7 +81,17 @@ const getValueByNumber = (obj, n) => {
   return obj[arrayKeysObj[n]];
 }
 
-console.log('Resposta Ex07');
+console.log('Resposta Ex07 expected: /n Matemática');
 console.log(getValueByNumber(lesson1, 0));
 
 // 8 - Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. 
+const verifyPair = (obj, key, value) => {
+  if (obj[key] === value) {
+    return true
+  }
+  return false
+}
+
+console.log('Resposta Ex08 expected: /n true /n false');
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
