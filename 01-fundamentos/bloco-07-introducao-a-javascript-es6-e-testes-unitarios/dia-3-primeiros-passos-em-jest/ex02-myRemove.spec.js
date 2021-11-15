133c0d57bcc8119402973245e719ddf5663ba6cf
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-15 08:00:06 -0300

    ex 2.3

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/ex02-myRemove.spec.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/ex02-myRemove.spec.js
@@ -22,3 +22,7 @@ it('2.1 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esper
 it('2.2 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
   expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
 })
+
+it('2.3 - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+  expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+})
