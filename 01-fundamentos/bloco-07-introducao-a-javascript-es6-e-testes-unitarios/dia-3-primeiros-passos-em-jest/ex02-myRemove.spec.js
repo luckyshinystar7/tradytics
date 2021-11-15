ed6c171797c610f46823d67bc1cf5d82f0142d9d
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-15 07:38:48 -0300

    finaliza ex 1.4

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/ex02-myRemove.spec.js
@@ -0,0 +1,16 @@
+// 2 - A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+//   2.1 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+//   2.2 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+//   2.3 - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
