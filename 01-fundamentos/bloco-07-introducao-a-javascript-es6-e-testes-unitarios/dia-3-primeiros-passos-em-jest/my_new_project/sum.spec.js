8be73095673c2872a233459cb72a50ace0e22258
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-15 04:44:13 -0300

    comecando no Jest

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/my_new_project/sum.spec.js
@@ -0,0 +1,5 @@
+// 1 - A função sum(a, b) retorna a soma do parâmetro a com o b
+//    1 - Teste se o retorno de sum(4, 5) é 9
+//    2 - Teste se o retorno de sum(0, 0) é 0
+//    3 - Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+//    4 - Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
