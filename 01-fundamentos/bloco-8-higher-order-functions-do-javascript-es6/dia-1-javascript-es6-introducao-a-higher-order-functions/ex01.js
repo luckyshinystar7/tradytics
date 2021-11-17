743e3435f5f26442a990a47968e9fc73534d5649
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-17 12:43:18 -0300

    aprimora ex01 com o uso do join() e utilizando callback propriamente

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/ex01.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/ex01.js
@@ -1,20 +1,26 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
 
-const newEmployees = () => {
-  const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  }
-  return employees;
-};
-
 const assignEmail = (empName) => {
   const nameLowerCase = empName.toLowerCase();
   const nameArray = nameLowerCase.split(' ');
-  const email = `nameArray[0]_nameArray[1]@trybe.com`;
+  // pior forma -> const email = `${nameArray[0]}_${nameArray[1]}@trybe.com`;
+  const email = `${nameArray.join('_')}@trybe.com`;
   return {
     nome: empName,
     email: email
   }
 }
+
+
+const newEmployees = (callback) => {
+
+
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(assignEmail));
