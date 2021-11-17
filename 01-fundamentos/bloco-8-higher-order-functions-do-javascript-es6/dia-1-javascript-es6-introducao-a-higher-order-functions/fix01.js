be94832c5860d79bf8ec361b82d40309f77e3309
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-17 11:30:06 -0300

    fiz03

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fix01.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fix01.js
@@ -6,6 +6,8 @@ const acordando = () => 'Acordando!!';
 const cafe = () => 'Bora tomar café!!';
 
 // 3 - Crie mais uma função que retorne a string 'Partiu dormir!!' ;
+const dormir = () => 'Partiu dormir!!';
+
 // 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
 
 
