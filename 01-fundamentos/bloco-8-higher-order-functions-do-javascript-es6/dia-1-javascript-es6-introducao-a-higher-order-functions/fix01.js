a0f8472ccc38c8f0c9e1631e00c445f1c1e1b8bd
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-17 11:29:34 -0300

    fix02

---
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fix01.js
@@ -0,0 +1,11 @@
+// Vamos praticar com os seguintes exercícios:
+// 1 - Crie uma função que retorne a string 'Acordando!!' ;
+const acordando = () => 'Acordando!!';
+
+// 2 - Crie outra função que retorne a string 'Bora tomar café!!' ;
+const cafe = () => 'Bora tomar café!!';
+
+// 3 - Crie mais uma função que retorne a string 'Partiu dormir!!' ;
+// 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
+
+
