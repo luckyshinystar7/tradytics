cbc16affb41f15f250aec078100346d663d60419
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-17 12:00:02 -0300

    finaliza exercicios de fixacao

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fix01.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fix01.js
@@ -1,6 +1,6 @@
 // Vamos praticar com os seguintes exercícios:
 // 1 - Crie uma função que retorne a string 'Acordando!!' ;
-const acordando = () => 'Acordando!!';
+const acordar = () => 'Acordando!!';
 
 // 2 - Crie outra função que retorne a string 'Bora tomar café!!' ;
 const cafe = () => 'Bora tomar café!!';
@@ -11,3 +11,6 @@ const dormir = () => 'Partiu dormir!!';
 // 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
 
 
+const doingThings = (state) => console.log(state());
+
+doingThings(dormir);
