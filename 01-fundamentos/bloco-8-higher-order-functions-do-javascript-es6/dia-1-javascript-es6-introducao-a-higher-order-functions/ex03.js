379d2f475d99acd66239146471ca5c7d0252e4b9
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-18 16:15:58 -0300

    ex03 concluido

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/ex03.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/ex03.js
@@ -4,13 +4,11 @@
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-//const notaFinal = (RIGHT_ANSWERS, STUDENT_ANSWERS, verifyanswers)
-
 const verifyAnswers = (array1, array2) => {
   let nota = 0;
-  for (i=0 ; i < array1.length ; i+=1) {
-    if (array2[i] === 'N.A') {}
-    else if (array1[i] === array2[i] ) {
+  for (i = 0; i < array1.length; i += 1) {
+    if (array2[i] === 'N.A') { }
+    else if (array1[i] === array2[i]) {
       nota += 1;
     } else {
       nota -= 0.5;
@@ -18,4 +16,8 @@ const verifyAnswers = (array1, array2) => {
   }
   return nota;
 }
-console.log (verifyAnswers(RIGHT_ANSWERS,STUDENT_ANSWERS));
+//console.log (verifyAnswers(RIGHT_ANSWERS,STUDENT_ANSWERS));
+
+const hofNotaFinal = (arr1, arr2, func) => func(arr1, arr2);
+
+console.log(hofNotaFinal(RIGHT_ANSWERS, STUDENT_ANSWERS, verifyAnswers));
