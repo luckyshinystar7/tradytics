58b9bd7681644744fbab0572c2f8833f4db9862f
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-19 12:12:41 -0300

    ex01 - parte 3 concluida

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/ex01.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/ex01.js
@@ -86,3 +86,21 @@ function smallerName() {
   return nameBook;
 }
 // console.log(smallerName());
+
+// 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  const result = books.find((value) => value.name.length === 26);
+};
+// console.log(getNamedBook());
