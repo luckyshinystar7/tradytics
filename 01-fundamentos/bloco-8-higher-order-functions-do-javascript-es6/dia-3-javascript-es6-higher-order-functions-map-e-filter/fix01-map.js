46a063120b74b91b6100c19d3a041e2807d57b6c
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-19 19:03:47 -0300

    iniciando ex fix01

---
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/fix01-map.js
@@ -0,0 +1,7 @@
+// Outras formas de usar o .map é unir dois arrays para criar um novo. Considere que você possui duas listas: o preço do primeiro produto ( Arroz ) é o primeiro elemento da lista prices ( 2.99 ), e assim por diante:
+
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+// Deseja-se juntá-las em apenas uma lista de objetos que fique dessa forma:
+// const listProducts = [{ Arroz: 2.99 },...]
+// Tente criar uma função que resolva esse problema. Lembre-se, também, que as funções passadas para as HOFs podem receber vários parâmetros, não só o elemento sobre o qual ela está sendo iterada! Use isso em seu favor.
