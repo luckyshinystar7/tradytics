8d1dd1197eee07affa630446521b0c5dccc0d802
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-19 10:25:30 -0300

    fix02-find

---
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fix02-find.js
@@ -0,0 +1,9 @@
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (string) => string.length === 5;
+
+f = names.find(findNameWithFiveLetters);
+
+console.log(f);
