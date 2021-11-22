ec396aa78aaa28234460e9f4d6413c9d0e91b11b
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-22 20:18:39 -0300

    trabalhando no ex01 com encadeamento de HOFs

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex01.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex01.js
@@ -6,13 +6,20 @@ const arrays = [
   [4, 5, 6],
 ];
 // For
-let arrayFinal = [];
-const reduz = (elemento) => {
-  for (let i = 0; i < elemento.length; i += 1) {
-    for (let j = 0; j < elemento[i].length; j += 1) {
-      arrayFinal.push(elemento[i][j]);
-    }
-  }
-}
-reduz(arrays);
-console.log(arrayFinal);
+// let arrayFinal = [];
+// const reduz = (elemento) => {
+//   for (let i = 0; i < elemento.length; i += 1) {
+//     for (let j = 0; j < elemento[i].length; j += 1) {
+//       arrayFinal.push(elemento[i][j]);
+//     }
+//   }
+// }
+// reduz(arrays);
+// console.log(arrayFinal);
+
+const reduce = arrays.reduce((acc, elemento) => {
+  acc.push(elemento);
+  return acc;
+}, [])
+
+console.log(reduce);
