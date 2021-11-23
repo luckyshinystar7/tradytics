2ac9999b4a8b1998653affc541a79964fe519fbc
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-23 10:12:02 -0300

    ex03

---
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex03.js
@@ -0,0 +1,79 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// 3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+
+// const averageAge = books.reduce((acc, book, i, array) => {
+//   const age = book.releaseYear - book.author.birthYear;
+//   acc += age;
+//   if (array.length - 1 === i) {
+//     return (acc / array.length)
+//   } else { return acc };
+// }, 0)
+
+// SEGUNDA FORMA DE RESOLVER:
+
+const totalAge = books.reduce((acc, book) => acc + (book.releaseYear - book.author.birthYear), 0);
+const averageAge = totalAge / books.length;
+
+console.log(averageAge);
