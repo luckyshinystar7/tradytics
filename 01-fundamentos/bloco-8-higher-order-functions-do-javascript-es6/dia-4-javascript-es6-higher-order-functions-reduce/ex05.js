bacc7109903690bb6df6572e676de4129ea5c7cb
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-23 11:29:29 -0300

    ex05 resolvido utilizando join split e reduce

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex05.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex05.js
@@ -5,16 +5,23 @@ const names = [
 ];
 
 // 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
-function containsA(array) {
-  let upperA = 0;
-  let lowerA = 0;
-  for (let i = 0; i < array.length; i += 1) {
-    for (let j = 0; j < array[i].length; j += 1) {
-      if (array[i][j] === 'A') { upperA += 1 };
-      if (array[i][j] === 'a') { lowerA += 1 };
-    }
-  }
-  return `Foi encontrado "A":${upperA} vezes e "a":${lowerA} vezes.`;
-}
 
-console.log(containsA(names));
+// RESOLUCAO COM FOR
+// function containsA(array) {
+//   let upperA = 0;
+//   let lowerA = 0;
+//   for (let i = 0; i < array.length; i += 1) {
+//     for (let j = 0; j < array[i].length; j += 1) {
+//       if (array[i][j] === 'A') { upperA += 1 };
+//       if (array[i][j] === 'a') { lowerA += 1 };
+//     }
+//   }
+//   return `Foi encontrado "A":${upperA} vezes e "a":${lowerA} vezes.`;
+// }
+
+// console.log(containsA(names));
+
+// RESOLUCAO COM JOIN SPLIT E REDUCE
+const containsA = names.join().split('').reduce((acc, curr) => (curr === 'a' || curr === 'A') ? acc + 1 : acc, 0)
+
+console.log(containsA);
