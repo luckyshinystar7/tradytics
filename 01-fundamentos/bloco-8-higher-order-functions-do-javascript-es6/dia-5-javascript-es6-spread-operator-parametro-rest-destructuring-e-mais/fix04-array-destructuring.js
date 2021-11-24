dc8fc477a4c7bff0010f774eadca66661d198e8e
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-24 13:13:10 -0300

    fix04.02 ignorando elementos no array destructuring

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fix04-array-destructuring.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fix04-array-destructuring.js
@@ -18,5 +18,13 @@ let bebida = 'arroz';
 // console.log(comida, animal, bebida); // arroz gato água
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
-[comida,animal,bebida] = [bebida,comida,animal];
-console.log(comida, animal, bebida);
+[comida, animal, bebida] = [bebida, comida, animal];
+//console.log(comida, animal, bebida);
+
+// 3 - array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares .
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+[, , , ...numerosPares] = numerosPares;
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
