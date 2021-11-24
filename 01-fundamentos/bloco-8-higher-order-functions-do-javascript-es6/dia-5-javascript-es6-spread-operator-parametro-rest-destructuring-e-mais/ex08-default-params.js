27f3f6f3d8e8435a2e4ed3a04bf2e26e6136fbaa
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-24 16:32:04 -0300

    ex08-default-params

---
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/ex08-default-params.js
@@ -0,0 +1,11 @@
+// 8 - Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+// Dica: use default params .
+
+// escreva greet abaixo
+
+const greet = (personName, greetings = 'Hi') => `${greetings} ${personName}`;
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
+
