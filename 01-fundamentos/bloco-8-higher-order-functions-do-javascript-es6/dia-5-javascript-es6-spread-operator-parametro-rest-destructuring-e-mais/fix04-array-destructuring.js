27a9b8e5f1c5a369c3727d42ac34d13f0b6b5993
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-24 12:17:24 -0300

    fix04.02 array.reassigning values

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fix04-array-destructuring.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fix04-array-destructuring.js
@@ -7,4 +7,16 @@ const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 // Produza o mesmo resultado acima, porém utilizando array destructuring
 const [stringSaudacao, funcSaudacao] = saudacoes;
 
-funcSaudacao(stringSaudacao);
+// funcSaudacao(stringSaudacao);
+
+// 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+// console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+[comida,animal,bebida] = [bebida,comida,animal];
+console.log(comida, animal, bebida);
