04d99c294e00ded25416813b0569bd7fcdd395b0
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-26 18:32:31 -0300

    terceiro teste em ex Fix05

--- 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fix05-testes-assincronos.test.js
+++ 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fix05-testes-assincronos.test.js
@@ -7,10 +7,22 @@ const { doesNotMatch } = require("assert");
 //   }, 500);
 // });
 
-test('Não deveria passar!', (done) => { // nao passa pois a function demora mais do que o esperado pelo teste
-  setTimeout(() => {
-    expect(10).toBe(5);
-    console.log('Deveria falhar!');
-    done();
-  }, 500);
-});
+// test('Não deveria passar!', (done) => { // nao passa pois a function demora mais do que o esperado pelo teste
+//   setTimeout(() => {
+//     expect(10).toBe(5);
+//     console.log('Deveria falhar!');
+//     done();
+//   }, 500);
+// });
+
+// test('Não deveria passar!', (done) => { // falso positivo pois o catch captura o erro e o try segue com a function
+//   setTimeout(() => {
+//     try {
+//       expect(10).toBe(5);
+//       console.log('Deveria falhar!');
+//       done();
+//     } catch (error) {
+//       done();
+//     }
+//   }, 500);
+// });
