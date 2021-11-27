88241b49c4cde9cb4dacfd3c9ba669808b39019d
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-27 08:35:39 -0300

    adicinando e testando ex07.js e ex07.test.js

--- 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/ex07.test.js
+++ 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/ex07.test.js
@@ -1,7 +1,15 @@
 // 7 - Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
 
-const uppercase = (str) => str.toUpperCase();
+// const uppercase = (str) => str.toUpperCase();
+
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
+};
 
 test('Verifica se funcao converte string para maiuscula', () => {
   expect(uppercase('eita')).toBe('EITA');
 });
+
+console.log(uppercase('eita'));
