1641cb657dc4cd933970a61ab4195a75d9cda511
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-11-29 18:56:09 -0300

    ex08 resolvido

--- 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/ex08.js
+++ 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/ex08.js
@@ -20,20 +20,20 @@ const pokemons = [
 
 function getPokemonDetails(filter, callback) {
   setTimeout(() => {
-    if (pokemons.find(filter) === undefined) {
+    if (pokemons.find((p) => p.name === filter) === undefined) {
       return callback(new Error('Não temos esse pokémon para você :('), null);
     }
-    const pokemon = pokemons.find(filter);
+    const pokemon = pokemons.find((p) => p.name === filter);
 
     const { name, type, ability } = pokemon;
 
     const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
 
-    callback(null, messageFromProfOak);
+    callback(messageFromProfOak);
   }, 2000);
 }
 
-getPokemonDetails();
+getPokemonDetails('Bulbasaur',console.log);
 
 module.exports = {
   getPokemonDetails,
