e0f53ae5d3732ca8f896e986c0cc4315fa7c4823
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-01 14:34:28 -0300

    exercicio 2 finalizado

--- 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/ex02-crypto.js
+++ 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/ex02-crypto.js
@@ -1,25 +1,28 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 const API_URL = 'https://api.coincap.io/v2/assets';
-// let log = [];
 
-// const append = (text) => {
-//   const h2 = document.querySelector('h2');
-//   h2.innerText = text;
-// }
+const append = (text) => {
+  const h1 = document.querySelector('h1');
+  const p = document.createElement('ul');
+  p.innerText = text;
+  p.style.fontSize = '0.5em';
+  p.style.margin = '0.5px';
+  p.style.fontWeight = 'normal';
+  h1.appendChild(p);
+}
 
 const fetchCoins = async () => {
-  // const myObject = {
-  //   method: 'GET',
-  //   headers: { 'Accept': 'application/json' }
-  // };
+
   try {
     const response = await fetch(API_URL)
     const data = await response.json();
     const element = data.data;
-    element.forEach((element) => console.log(`${element.name} (${element.symbol}): ${element.priceUsd}`));
+    // element.forEach((element) => console.log(`${element.name} (${element.symbol}): ${element.priceUsd}`));
+    element.forEach((element) => append(`${element.name} (${element.symbol}): ${element.priceUsd}`));
 
   } catch (error) {
-    console.log(`Algo deu errado :( \n${error}`);
+    append(`Algo deu errado :( \n${error}`);
+    // console.log(`Algo deu errado :( \n${error}`);
   }
 
   // return result;
