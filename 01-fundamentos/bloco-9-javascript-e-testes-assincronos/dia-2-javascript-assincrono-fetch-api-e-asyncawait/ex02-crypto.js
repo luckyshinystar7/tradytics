e24d773475ded16c6c20e3742bcf09c9d82dca71
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-01 12:11:01 -0300

    funcoes fetch e append criadas

--- 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/ex02-crypto.js
+++ 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/ex02-crypto.js
@@ -4,3 +4,15 @@ const append = (text) => {
   const h2 = document.querySelector('h2');
   h2.innerText = text;
 }
+
+const fetchJoke = () => {
+  // const myObject = {
+  //   method: 'GET',
+  //   headers: { 'Accept': 'application/json' }
+  // };
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => append(data));
+};
+
+fetchJoke();
