842f197b6d919f308ce0e40408ef36a111615768
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-01 11:56:34 -0300

    ex01 dad jokes running

--- 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/ex01-dad-jokes.js
+++ 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/ex01-dad-jokes.js
@@ -1,8 +1,19 @@
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const append = (text) => {
+  const h2 = document.querySelector('h2');
+  h2.innerText = text;
+}
+
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => append(data.joke));
 };
 
-window.onload = () => fetchJoke();
+fetchJoke();
