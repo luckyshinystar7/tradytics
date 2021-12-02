b322b436d47221cb6ea6bca6211606132fa8904a
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-02 11:44:54 -0300

    ex01 resolvido com sucesso utilizando resolves

--- 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/ex01.test.js
+++ 01-fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/ex01.test.js
@@ -1,4 +1,4 @@
-// 1 - O código abaixo busca no GitHub de um usuário, de acordo com a URL, seus repositórios e retorna uma lista como resultado. Dada a URL 'https://api.github.com/orgs/tryber/repos' , faça um teste que verifique que os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' se encontram nessa lista.
+// 1 - O código abaixo busca no GitHub de um usuário, de acordo com a URL, seus repositórios e retorna uma lista como resultado. Dada a URL 'https://api.github.com/orgs/tryber/repos' , faça um teste que verifique que os repositórios 'sd-04-block6-project-facebook-signup-tests' e 'sd-03-block9-project-zoo-functions-tests' se encontram nessa lista.
 // githubApi.test.js
 
 const url = 'https://api.github.com/orgs/tryber/repos';
@@ -11,3 +11,11 @@ const getRepos = (url) => (
 );
 
 // implemente seus testes aqui
+
+test('Verifica se os repositórios "sd-01-week4-5-project-todo-list" está na lista', () => {
+  return expect(getRepos(url)).resolves.toContain('sd-04-block6-project-facebook-signup-tests');
+});
+
+test('Verifica se os repositórios "sd-01-week4-5-project-todo-list" está na lista', () => {
+  return expect(getRepos(url)).resolves.toContain('sd-03-block9-project-zoo-functions-tests');
+});
