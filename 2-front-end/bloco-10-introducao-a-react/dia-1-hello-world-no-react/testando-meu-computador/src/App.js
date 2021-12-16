0a600e3d7768c572014815f63a2b7ceedb10d6b7
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 16:59:32 -0300

    Finaliza exercicio de fixacao Fix01, criando um elemento JSX que recebe o valor "Hello, JSX" em uma constante textJSX e incorporando ele em uma tag h1

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
@@ -2,39 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+
   // function helloWorld (nome, sobrenome) {
   //   return <h1>Hello, {`${nome} ${sobrenome}`}</h1>;
   // }
-  
+
   // const element = helloWorld("Jorge", "Maravilha");
   // const container = <div>{element}</div>;
 
   const nome = 'Jorge Maravilha';
-const classe = 'hello-class';
-const element = <h1 className={classe}>Hello, {nome}</h1>;
+  const classe = 'hello-class';
+  const element = <h1 className={classe}>Hello, {nome}</h1>;
+  const textJSX = <h1>Hello, JSX</h1>; // exercicio de fixacao FIX01
 
-return (
+  return (
 
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      {element}
-      <p>
-        Edit <code>src/App.js</code> and save to reload
-      </p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {element};
+        {textJSX};
+        <p>
+          Edit <code>src/App.js</code> and save to reload
+        </p>
 
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
