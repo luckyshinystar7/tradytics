e60557e1bf44cd496baf59a7207d53f1a1c1ffd1
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 16:49:37 -0300

    chamando uma function incorporando elementos JGX

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 function App() {
   
   
-const nome = 'Jorge Maravilha';
-const element = <h1>Hello, {nome}</h1>;
+
+  function nomeCompleto (nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+  }
+  
+  const element = <h1>Hello, {nomeCompleto("Jorge", "Maravilha")}</h1>;
 
 return (
 
