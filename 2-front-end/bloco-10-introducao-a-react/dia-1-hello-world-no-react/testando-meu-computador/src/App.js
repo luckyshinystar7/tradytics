e0934f7e3cf4cbed663b5860cdaf450a1436334f
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 17:55:57 -0300

    manipulacao CSS com react

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
@@ -20,8 +20,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {element};
-        {textJSX};
+        {element}
+        {textJSX}
         <p>
           Edit <code>src/App.js</code> and save to reload
         </p>
