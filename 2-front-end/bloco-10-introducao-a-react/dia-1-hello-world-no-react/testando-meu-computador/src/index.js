d92a3742c51f6c4ce8b935d533c501223a96dbd2
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 17:37:00 -0300

    testando alteracao no codigo utilizando uma function que manipula o ReactDOM.render

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/index.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// Testando alteração no codigo utilizando o ReactDOM.render
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, ReactDOM.render</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
