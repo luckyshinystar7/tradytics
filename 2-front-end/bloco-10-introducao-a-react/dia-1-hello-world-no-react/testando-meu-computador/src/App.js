bf09068b6c0c37593672b29f5ff34084a7ee1f8a
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 16:48:07 -0300

    Incorporando expressoes no JGX, aplicando tags dentro de variaveis

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
@@ -2,28 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const nome = 'Jorge Maravilha';
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  
+  
+const nome = 'Jorge Maravilha';
+const element = <h1>Hello, {nome}</h1>;
 
-        <h1>Hello, {nome}</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
+return (
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <h1>Hello, {element}</h1>
+      <p>
+        Edit <code>src/App.js</code> and save to reload
+      </p>
+
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 }
 
 export default App;
