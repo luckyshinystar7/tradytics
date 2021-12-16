c7782955f315349665918b9e8a89b1cdd04cf924
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 18:38:14 -0300

    transfere function de teste com atualizacao em tempo real para novo componente, ainda nao deu certo, irei voltar nesse componente quando estiver com mais bagagem em React e manipulacao de classes e componentes

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
@@ -23,10 +23,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         {element}
         {textJSX}
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-
+        
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/Time.js
@@ -0,0 +1,21 @@
+// import React from "react";
+
+// Testando alteração no codigo utilizando o ReactDOM.render como componente
+
+// class TimeRightNow extends React.Component {
+
+//   function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, ReactDOM.render</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000);
+  
+// }
+
+// export default TimeRightNow;
