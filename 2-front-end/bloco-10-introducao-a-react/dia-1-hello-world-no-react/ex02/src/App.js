989e0a1460a039d88b66eef2e03ae23d82a486d6
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 21:32:12 -0300

    Ex02: Remove arquivos desnecessarios instalados automaticamente com o pacote React

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex02/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex02/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Content from './Content';
 
 function App() {
   return (
@@ -8,18 +8,8 @@ function App() {
     < div className="App" >
       < Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        < Content />
+       
       </header>
     </div >
   );
