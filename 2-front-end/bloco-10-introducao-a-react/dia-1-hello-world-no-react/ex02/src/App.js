48baf770a5c0b4d7bb5411245f4a2050ca8a5220
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 21:13:46 -0300

    Ex02: Importa o componente Header corretamente dentro do App e renderiza o mesmo

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex02/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex02/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
+
+    < div className="App" >
+      < Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div >
   );
 }
 
