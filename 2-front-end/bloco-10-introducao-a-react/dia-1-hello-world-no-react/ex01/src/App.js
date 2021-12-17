c7716fd87a44b2411019836ff8347b4d8d9dda27
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 21:10:41 -0300

    transfere Header para a pasta correta, do Exercicio 02

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex01/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex01/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './Header';
 
 const ToDoList = ['lavar o gato', 'alimentar o gato', 'limpar o banheiro do gato', 'comprar comida para o gato', 'dar atenção ao gato'];
 const Task = (value) => {
