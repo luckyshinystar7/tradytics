62619e172acb46dd5c7bba778b8d356c125bfa0b
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 21:24:12 -0300

    Ex02: Adiciona array com lista de conteudos

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex02/src/Content.jsx
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex02/src/Content.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
+const conteudos = [
+  {
+    conteudo: "High Order Functions",
+    bloco: 8,
+    status: "Aprendido",
+  },
+  {
+    conteudo: "Composicao de Componentes",
+    bloco: 11,
+    status: "Aprendendo",
+  },
+  {
+    conteudo: "Composicao de Estados",
+    bloco: 12,
+    status: "Aprenderei",
+  },
+  {
+    conteudo: "Redux",
+    bloco: 16,
+    status: "Aprenderei",
+  },
+];
+
 class Content extends React.Component {
   render() {
     return <h1>Content</h1>;
