67b4d8c2911d7e4e831622620a7b11acb3d63b26
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-12-16 22:02:23 -0300

    Ex02: Importa e renderiza Footer criado anteriormente

--- 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex02/src/App.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex02/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       < Header />
       <header className="App-header">
         < Content />
-       
+       < Footer />
       </header>
     </div >
   );
