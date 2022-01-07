e2d66dc4791e3259a66274164f12e922afa26f37
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-07 16:13:07 -0300

    realizado destructuring no objeto user que veio dentro da props para melhorar legibilidade do codigo

--- 2-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fix-02/src/UserProfile.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fix-02/src/UserProfile.js
@@ -3,12 +3,15 @@ import React from "react";
 import Image from "./Image";
 
 class UserProfile extends React.Component {
+
   render() {
+    const { name, email, avatar } = this.props.user;
+
     return (
       <div>
-        <p> {this.props.user.name} </p>
-        <p> {this.props.user.email} </p>
-        <Image source={this.props.user.avatar} alternativeText="User avatar" />
+        <p> {name} </p>
+        <p> {email} </p>
+        <Image source={avatar} alternativeText="User avatar" />
       </div>
     );
   }
