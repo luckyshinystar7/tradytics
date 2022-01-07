4857f8377f51d243612a29fcc7bfa090e0eb7694
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-07 17:08:24 -0300

    Definindo os PropTypes

--- 2-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fix-02/src/UserProfile.js
+++ 2-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fix-02/src/UserProfile.js
@@ -1,11 +1,13 @@
 // arquivo UserProfile.js
 import React from "react";
 import Image from "./Image";
+import PropTypes from 'prop-types';
 
 class UserProfile extends React.Component {
 
   render() {
     const { name, email, avatar } = this.props.user;
+    // const { user: { name, email, avatar } } = this.props; // mesma coisa que a linha de cima
 
     return (
       <div>
@@ -15,6 +17,25 @@ class UserProfile extends React.Component {
       </div>
     );
   }
+}
+
+// UserProfile.propTypes = {
+//   user: PropTypes.object.isRequired
+// }
+
+// UserProfile.propTypes = {  // Props com destructuring
+//   name: PropTypes.string.isRequired,
+//   email: PropTypes.number,
+//   avatar: PropTypes.string
+// }
+
+UserProfile.propTypes = { // Props sem Destructuring
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string,
+    email: PropTypes.string,
+    avatar: PropTypes.string
+  })
 }
 
 export default UserProfile;
