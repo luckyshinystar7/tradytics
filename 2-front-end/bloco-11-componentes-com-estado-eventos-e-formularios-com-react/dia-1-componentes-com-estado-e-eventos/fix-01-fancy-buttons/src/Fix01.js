da694a099666b846e6ba0fce2cd8a0a805191f02
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-08 15:54:52 -0300

    Fix02- conhecendo a manipulacao do contructor

--- 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fix-01-fancy-buttons/src/Fix01.js
+++ 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fix-01-fancy-buttons/src/Fix01.js
@@ -9,10 +9,9 @@ class Fix01 extends React.Component {
     /* No React, precisamos dizer explicitamente que queremos uma função da nossa classe
     através da sintaxe `this.minhaFuncao` para usá-la num evento */
     return <>
-      <button onClick={this.handleClick} >Oi</button>
-      <button onClick={() => console.log("Olá")} >Ola</button>
-      <button onClick={() => console.log("Tchau")} >Tchau</button>
-      <button type="button" onClick={this.handleClick}>Meu botão</button>
+      <button type="button" onClick={this.handleClick} >Oi</button>
+      <button type="button" onClick={() => console.log("Olá")} >Ola</button>
+      <button type="button" onClick={() => console.log("Tchau")} >Tchau</button>
     </>
   }
 }
