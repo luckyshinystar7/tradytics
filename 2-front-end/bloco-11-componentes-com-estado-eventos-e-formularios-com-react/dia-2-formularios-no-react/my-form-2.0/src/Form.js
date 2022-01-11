8ea0d680f5774f396335e3a94d818481aae8c961
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-11 18:54:28 -0300

    Inicia exercicio 11.2/ criando um formulario em React // componente Form criado

---
+++ 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/Form.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function Form() {
+  return (
+    <div className="App">
+      <fieldset>
+        Nome <input type="text" required="true" ></input>
+      </fieldset>
+      
+    </div>
+  );
+}
+
+export default Form;
