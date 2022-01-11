81f35caef788fe1fb6aa1ef7bf4f4860b4986c87
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-11 15:26:33 -0300

    adiciona 2 validacoes no componente EstadoFavorito

--- 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/formularios-react/src/EstadoFavorito.js
+++ 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/formularios-react/src/EstadoFavorito.js
@@ -5,14 +5,25 @@ class EstadoFavorito extends Component {
   render() {
     const { value, handleChange } = this.props;
 
+    let maior20 = undefined;
+    if (value.length > 20) maior20 = "maior que 20";
+
+    let maior30 = undefined;
+    if (value.length > 30) maior30 = " e agora maior que 30 também";
+
+
     return (
-      <label>
-        Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
-        <textarea
-          name="estadoFavorito"
-          value={value}
-          onChange={handleChange} />
-      </label>
+      <>
+        <label>
+          Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
+          <textarea
+            name="estadoFavorito"
+            value={value}
+            onChange={handleChange} />
+        </label>
+        <span>{maior20 ? maior20 : ''}</span>
+        <span>{maior30 ? maior30 : ''}</span>
+      </>
     )
   }
 
