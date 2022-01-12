3feba7df789ad6cb057fae3f53e40c0cb11f09b7
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-12 17:01:50 -0300

    adiciona solution para exercicio proposto

--- 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/index.js
+++ 2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Form from './Form';
+import App from '../gabarito/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Form />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
