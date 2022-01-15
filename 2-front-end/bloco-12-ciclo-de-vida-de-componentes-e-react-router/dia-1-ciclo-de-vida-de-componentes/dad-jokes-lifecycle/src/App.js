18ac5ec5afeb2d871afdd521af96f0138c705603
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-15 19:03:10 -0300

    Cria exercicio de fixacao com componente DadJoke

---
+++ 2-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/dad-jokes-lifecycle/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import DadJoke from './components/DadJoke';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <DadJoke />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
