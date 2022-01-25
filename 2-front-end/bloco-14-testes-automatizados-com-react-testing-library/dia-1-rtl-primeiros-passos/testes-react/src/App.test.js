c3d3314d2c644ac183ab3468c379ec9460052b91
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-25 15:18:07 -0300

    adiciona teste para o botao de enviar utilizando data-testid

--- 2-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
+++ 2-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -9,14 +9,16 @@ test('Verificando se existe o campo Email.', () => {
   expect(inputEmail).toHaveProperty('type', 'email');
 });
 
-// test('Verificando se existe um botão', () => {
-//   const { getByRole } = render(<App />);
-//   const btn = getByRole('button');
-//   expect(btn).toBeInTheDocument();
-// });
-
 test('Verificando se existem dois botões', () => {
   render(<App />);
   const buttons = screen.getAllByRole('button');
   expect(buttons).toHaveLength(2);
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('btn-send');
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend).toHaveProperty('type', 'button');
+  expect(btnSend).toHaveValue('Enviar');
 });
