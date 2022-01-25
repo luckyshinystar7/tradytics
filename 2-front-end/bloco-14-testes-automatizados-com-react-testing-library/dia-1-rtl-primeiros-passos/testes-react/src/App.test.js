cb2e896b104a786403c2b5c7b46919cd3ae69c13
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-01-25 15:12:30 -0300

    Cria testes para verificar a existencia do campo email e se tem 2 botoes na tela

---
+++ 2-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('Verificando se existe o campo Email.', () => {
+  render(<App />);
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
+
+// test('Verificando se existe um botão', () => {
+//   const { getByRole } = render(<App />);
+//   const btn = getByRole('button');
+//   expect(btn).toBeInTheDocument();
+// });
+
+test('Verificando se existem dois botões', () => {
+  render(<App />);
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(2);
+});
