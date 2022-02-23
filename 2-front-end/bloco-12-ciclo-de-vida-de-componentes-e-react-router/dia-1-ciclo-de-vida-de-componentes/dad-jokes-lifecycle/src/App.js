0f84306451e4c7dd21ede1f06201b1850494e9c7
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-02-22 21:00:42 -0300

    Refaz o componente DadJoke em um novo componente DadJokeRevisando colocando novamente em pratica os aprendizados sobre manipulacao do state em funcoes asincronas

--- 2-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/dad-jokes-lifecycle/src/App.js
+++ 2-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/dad-jokes-lifecycle/src/App.js
@@ -2,10 +2,12 @@ import DadJoke from './components/DadJoke';
 import RickAndMorty from './rickAndMorty/RickAndMorty';
 import Counter from './components/Counter';
 import RandomUser from './components/randomUser';
+import DadJokeRevisando from './components/DadJokeRevisando';
 
 function App() {
   return (
-    <DadJoke />
+    <DadJokeRevisando />
+    // <DadJoke />
     // <RickAndMorty />
     // <Counter />
     // <RandomUser />
