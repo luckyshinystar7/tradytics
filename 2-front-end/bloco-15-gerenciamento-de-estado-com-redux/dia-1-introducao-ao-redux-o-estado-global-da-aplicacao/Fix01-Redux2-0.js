47d70d92ead7a19a18b834aea44ba722344d106e
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-03-23 18:07:28 -0300

    adiciona exemplo e orientaçao sobre o subscribe

--- 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/Fix01-Redux2-0.js
+++ 2-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/Fix01-Redux2-0.js
@@ -40,7 +40,16 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 // ATENÇÃO - AO ACESSAR UM STORE QUE UTILIZA UM COMBINE REDUCER, LEMBRAR DE ESPECIFICAR O REDUCER NA BUSCA ex: state.MEUREDUCER.email e nao state.email
+
 //{
 // meuReducer: {/_estado do meuReducer_/},
 // outroReducer: {/_estado do meuOutroReducer_/,}
 //}
+
+// SUBSCRIBE - é um listener que executa uma callback sempre que uma action é despachada.
+
+// Nesse exemplo ele imprime um log quando ocorre mudança no store
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
