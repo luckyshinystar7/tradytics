286fb28cf9929db8607ec98cc209ff0800410e93
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-25 18:19:10 -0300

    Fix03 - Passo 2

--- 3-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Fix03-Concat.sql
+++ 3-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Fix03-Concat.sql
@@ -7,4 +7,6 @@
     
 # 2. Na tabela sakila.film, crie uma query que exiba o título do filme e sua classificação indicativa (PG, G, NC-17) em apenas uma coluna.
 	# Dê a ela o nome Classificação. Não se esqueça de deixar um espaço entre as palavras para que fiquem legíveis.
+    SELECT CONCAT(title, ' - ', rating) AS "Classificacao" FROM sakila.film;
+    
 # 3. Na tabela sakila.address, monte uma query que exiba a rua e o distrito de cada registro em uma coluna apenas, e dê a essa coluna o nome Endereço.
