43b00bddb68aa3738b9118f4ace979e32bf4d236
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-25 16:50:53 -0300

    Fix02 - Passo 2

--- 3-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Fix02.sql
+++ 3-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Fix02.sql
@@ -5,6 +5,8 @@ USE sakila;
 	SELECT * FROM city;
     
 # 2. Escreva uma query que exiba apenas as colunas first_name e last_name da tabela customer;
+	SELECT first_name, last_name FROM customer;
+    
 # 3. Escreva uma query que exiba todas as colunas da tabela rental;
 # 4. Escreva uma query que exiba o título, a descrição e a data de lançamento dos filmes registrados na tabela film;
 # 5. Utilize o SELECT para explorar todas as tabelas do banco de dados.
