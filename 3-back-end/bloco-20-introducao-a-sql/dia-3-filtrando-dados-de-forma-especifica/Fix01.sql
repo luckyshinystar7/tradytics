945538a12286522c4c78be5c0de86cfa90380c52
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-26 11:35:17 -0300

    Fix01 - Passos 1 a 3

---
+++ 3-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/Fix01.sql
@@ -0,0 +1,23 @@
+-- Entre no banco de dados sakila e siga as instruções (e guarde as queries para conferir posteriormente):
+-- 1. Precisamos identificar os dados do cliente com o e-mail LEONARD.SCHOFIELD@sakilacustomer.org. As informações podem ser encontradas na tabela customer
+	select * from sakila.customer
+		where email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
+    
+-- 2. Precisamos de um relatório dos nomes dos clientes, em ordem alfabética, que não estão mais ativos no nosso sistema e pertencem à loja com o id = 2, e não inclua o cliente KENNETH no resultado. As informações podem ser encontradas na tabela customer
+	select * from sakila.customer
+		where active = 0
+		and store_id = 2
+		order by first_name;
+
+-- 3. O setor financeiro quer saber título, descrição, ano de lançamento e valor do custo de substituição (replacement_cost), dos 100 filmes com o maior custo de substituição, do valor mais alto ao mais baixo, entre os filmes feitos para menores de idade e que têm o custo mínimo de substituição de $18,00 dólares. Em caso de empate, ordene em ordem alfabética pelo título. As informações podem ser encontradas na tabela film
+	select title, description, release_year, replacement_cost from sakila.film
+    where rating <> 'NC-17'
+    and replacement_cost > 18
+    order by replacement_cost desc, title
+    limit 100;    
+
+-- 4. Quantos clientes estão ativos e na loja 1? As informações podem ser encontradas na tabela customer
+
+-- 5. Mostre todos os detalhes dos clientes que não estão ativos na loja 1. As informações podem ser encontradas na tabela customer
+
+-- 6. Precisamos descobrir quais são os 50 filmes feitos apenas para adultos com a menor taxa de aluguel, para que possamos fazer uma divulgação melhor desses filmes. Em caso de empate, ordene em ordem alfabética pelo título. As informações podem ser encontradas na tabela film
