a13121c66ca24928340e9d51852ee5e3c51d668f
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-26 20:16:52 -0300

    Ex05 - Passos 4 a 7

--- 3-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/Ex05-AllTogether.sql
+++ 3-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/Ex05-AllTogether.sql
@@ -15,9 +15,19 @@
     where Fornecedor like '%N%';
     
 -- 4. Avante, falta pouco! Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene esses resultados em ordem alfabética decrescente.
+	select * from Fornecedores
+    where name like '%LTDA%'
+    order by name desc;
 
 -- 5. Agora, faça uma consulta para exibir o número de empresas (fornecedores) que contém a letra F no código.
-
+	select count(*) as 'Fornecedores com codigo contendo "F"' from Fornecedores
+    where code like '%f%';
+    
 -- 6. Quase lá! Agora escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00. Ordene os resultados por ordem crescente.
-
+	select * from Fornecimentos
+    where Preco between 15.00 and 40.00
+    order by Preco asc;
+    
 -- 7. Ufa! Por fim, faça uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019.
+	select * from Vendas
+    where date(order_date) between '2018-04-15 00:00:00' and '2019-07-30 23:59:59';
