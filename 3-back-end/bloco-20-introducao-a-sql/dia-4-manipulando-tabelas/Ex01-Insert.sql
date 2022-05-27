f2831ba08b20c19cb33e42609f811fd64c18bb16
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-26 21:23:24 -0300

    Ex01 - Passos 2 ao 4

--- 3-back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/Ex01-Insert.sql
+++ 3-back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/Ex01-Insert.sql
@@ -5,13 +5,20 @@
 	# use sakila;
     insert into staff (first_name, last_name, address_id, email, store_id, `active`, username, `password`)
     values ('Josh', 'Mayer', 2, 'josh.mayer@sakilastaff.com', 2, 1, 'Josh', 'J*ghd');
-    select * from staff;
 
 -- 2. Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione Table Inspector. Clique na aba columns e verifique quais campos aceitam nulos para te guiar. Lembre-se de que valores que são gerados automaticamente não precisam ser inseridos manualmente. Boa exploração!
-
+	# RESP: Os campos password e email aceitam valor nulo
+    
 -- 3. Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query.
-
+	insert into staff (first_name, last_name, address_id, store_id, `active`, username)
+    values 
+		('Michael', 'Jackson', 5, 2, 1, 'Jackson'),
+        ('Mike', 'Caruzo', 6, 2, 1, 'MikeC');
+        
 -- 4. Selecione os cinco primeiros nomes e sobrenomes da tabela sakila.customer e cadastre essas pessoas como atores na tabela sakila.actor.
+	insert into sakila.actor (first_name, last_name)
+    	select first_name, last_name from sakila.staff
+        limit 3;
 
 -- 5. Cadastre três categorias de uma vez só na tabela sakila.category.
 
