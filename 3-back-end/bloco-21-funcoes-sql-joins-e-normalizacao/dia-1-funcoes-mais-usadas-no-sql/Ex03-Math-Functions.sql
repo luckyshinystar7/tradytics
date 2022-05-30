03f69f4991358334bfeefd93fa14fe97c5e30467
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-30 19:39:14 -0300

    Ex03 - Steps 4 to 7

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex03-Math-Functions.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex03-Math-Functions.sql
@@ -11,9 +11,14 @@
 	SELECT 220 MOD 12 AS 'Lugares sobrando';
 
 -- 4. Monte uma query que gere um valor entre 15 e 20.
+	SELECT ROUND(15 + (RAND() * 5));
 
 -- 5. Monte uma query que exiba o valor arredondado de 15.7515971 com uma precisão de 5 casas decimais.
-
+	SELECT ROUND(15.7515971, 5);
+    
 -- 6. Estamos com uma média de 39.494 de vendas de camisas por mês. Qual é o valor aproximado para baixo dessa média?
+	SELECT FLOOR(39.494);
 
 -- 7. Temos uma taxa de inscrição de 85.234% no curso de fotografia para iniciantes. Qual é o valor aproximado para cima dessa média?
+	SELECT CEIL(85.234);
+    
