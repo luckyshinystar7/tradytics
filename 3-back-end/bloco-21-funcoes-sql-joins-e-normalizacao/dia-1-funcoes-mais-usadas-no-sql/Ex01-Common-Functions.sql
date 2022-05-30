abecae3738c27827f7332617f77c82cf5132c0c9
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-30 11:48:27 -0300

    Ex01 - Step 1

---
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex01-Common-Functions.sql
@@ -0,0 +1,21 @@
+-- Exercicio 01 - Common Functions
+
+-- 1. Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.
+	SELECT UCASE('trybe');
+    
+-- 2. Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?'.
+
+-- 3. Utilizando uma query, encontre quantos caracteres temos em 'Uma frase qualquer'.
+
+-- 4. Extraia e retorne apenas a palavra "JavaScript" da frase 'A linguagem JavaScript está entre as mais usadas'.
+
+-- 5. Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.
+
+-- SELECT UCASE(title) FROM sakila.film LIMIT 10;
+-- SELECT LCASE(title) FROM sakila.film LIMIT 10;
+-- SELECT REPLACE(title, 'ACADEMY', 'FOO') FROM sakila.film WHERE film_id = 1;
+-- SELECT LEFT(title, 7) FROM sakila.film WHERE film_id = 1;
+-- SELECT RIGHT(title, 8) FROM sakila.film WHERE film_id = 1;
+-- SELECT CHAR_LENGTH(title) FROM sakila.film WHERE film_id = 1;
+-- SELECT SUBSTRING(title, 5, 2) FROM sakila.film WHERE film_id = 1;
+-- SELECT SUBSTRING(title, 5) FROM sakila.film WHERE film_id = 1;
