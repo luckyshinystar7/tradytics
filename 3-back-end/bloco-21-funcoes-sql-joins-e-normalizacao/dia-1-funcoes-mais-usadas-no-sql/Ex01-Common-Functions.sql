22f1a5e08c19df56ec7331fa600bbcece3d412b7
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-30 11:56:05 -0300

    Ex01 - Step 5

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex01-Common-Functions.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex01-Common-Functions.sql
@@ -13,7 +13,7 @@
 	SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13, 10);
 
 -- 5. Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.
-
+	SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
 
 -- EXEMPLOS
 
