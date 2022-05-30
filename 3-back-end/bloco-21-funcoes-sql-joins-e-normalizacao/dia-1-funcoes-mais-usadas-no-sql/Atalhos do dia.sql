ae6302b909235d416c19442cc7e65bdfb4408879
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-30 20:46:39 -0300

    Ex05 - Steps 1 to 5

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Atalhos do dia.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Atalhos do dia.sql
@@ -26,3 +26,69 @@ SELECT SUBSTRING('Oi, eu sou uma string', 5, 2);
 -- Se a quantidade de caracteres a extrair não for definida,
 -- então a string será extraída do índice inicial definido, até o seu final
 SELECT SUBSTRING('Oi, eu sou uma string', 5);
+
+-- Sintaxe:
+SELECT IF(condicao, valor_se_verdadeiro, valor_se_falso);
+
+SELECT IF(idade >= 18, 'Maior de idade', 'Menor de Idade')
+FROM pessoas;
+
+SELECT IF(aberto, 'Entrada permitida', 'Entrada não permitida')
+FROM estabelecimentos;
+
+-- Exemplo utilizando o banco sakila:
+SELECT first_name, IF(active, 'Cliente Ativo', 'Cliente Inativo') AS status
+FROM sakila.customer
+LIMIT 20;
+
+-- Sintaxe:
+SELECT CASE
+  WHEN condicao THEN valor
+  ELSE 'valor padrao'
+END;
+
+SELECT
+    nome,
+    nivel_acesso,
+    CASE
+        WHEN nivel_acesso = 1 THEN 'Nível de acesso 1'
+        WHEN nivel_acesso = 2 THEN 'Nível de acesso 2'
+        WHEN nivel_acesso = 3 THEN 'Nível de acesso 3'
+        ELSE 'Usuário sem acesso'
+    END AS nivel_acesso
+FROM permissoes_usuario;
+
+-- Exemplo utilizando a tabela sakila.film:
+SELECT
+    first_name,
+    email,
+    CASE
+        WHEN email = 'MARY.SMITH@sakilacustomer.org' THEN 'Cliente de baixo valor'
+        WHEN email = 'PATRICIA.JOHNSON@sakilacustomer.org' THEN 'Cliente de médio valor'
+        WHEN email = 'LINDA.WILLIAMS@sakilacustomer.org' THEN 'Cliente de alto valor'
+        ELSE 'não classificado'
+    END AS valor
+FROM sakila.customer
+LIMIT 10;
+
+SELECT CURRENT_DATE(); -- YYYY-MM-DD
+SELECT NOW(); -- YYYY-MM-DD HH:MM:SS
+
+-- 30, ou seja, a primeira data é 30 dias depois da segunda
+SELECT DATEDIFF('2020-01-31', '2020-01-01');
+
+-- -30, ou seja, a primeira data é 30 dias antes da segunda
+SELECT DATEDIFF('2020-01-01', '2020-01-31');
+
+-- -01:00:00, ou seja, há 1 hora de diferença entre os horários
+SELECT TIMEDIFF('08:30:10', '09:30:10');
+
+-- -239:00:00, ou seja, há uma diferença de 239 horas entre as datas
+SELECT TIMEDIFF('2021-08-11 08:30:10', '2021-08-01 09:30:10');
+
+-- Usando a coluna replacement_cost (valor de substituição), vamos encontrar:
+SELECT AVG(replacement_cost) FROM sakila.film; -- 19.984000 (Média entre todos registros)
+SELECT MIN(replacement_cost) FROM sakila.film; -- 9.99 (Menor valor encontrado)
+SELECT MAX(replacement_cost) FROM sakila.film; -- 29.99 (Maior valor encontrado)
+SELECT SUM(replacement_cost) FROM sakila.film; -- 19984.00 (Soma de todos registros)
+SELECT COUNT(replacement_cost) FROM sakila.film; -- 1000 registros encontrados (Quantidade)
