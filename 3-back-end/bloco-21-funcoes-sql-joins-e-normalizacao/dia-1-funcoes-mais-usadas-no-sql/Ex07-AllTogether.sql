de3f94ee4e18829eb5025fcdea15431091170d3d
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-31 20:10:35 -0300

    Ex07 - Steps 7 and 8

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex07-AllTogether.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex07-AllTogether.sql
@@ -31,8 +31,16 @@
     WHERE JOB_ID = 'IT_PROG';
 
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão (JOB_ID).
+	SELECT JOB_ID, SUM(SALARY) AS TOTAL_SALARY
+    FROM hr.employees
+    group by JOB_ID
+    ORDER BY TOTAL_SALARY DESC;
 
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras (IT_PROG).
+	SELECT JOB_ID, SUM(SALARY) AS SUM_SALARY
+    FROM hr.employees
+    GROUP BY JOB_ID
+    HAVING JOB_ID = 'IT_PROG';
 
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras (IT_PROG).
 
