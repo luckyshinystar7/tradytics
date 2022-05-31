93f25abd018766ecbb0e5cf5167a971f3d866466
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-31 18:51:33 -0300

    Ex06 - Steps 5 and 6

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex06-GroupByHaving.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex06-GroupByHaving.sql
@@ -21,3 +21,24 @@
     FROM sakila.address
     GROUP BY district
     ORDER BY count(*) desc;
+    
+-- 5. Usando a query a seguir, modifique-a de forma que exiba apenas as durações médias que estão entre 115.0 a 121.50. Além disso, dê um alias (apelido) à coluna gerada por AVG(length), de forma que deixe a query mais legível. Finalize ordenando os resultados de forma decrescente.
+--    SELECT rating, AVG(length)
+--    FROM sakila.film
+--    GROUP BY rating;
+	SELECT rating, AVG(length) AS avg_length
+	FROM sakila.film
+	GROUP BY rating
+    HAVING avg_length BETWEEN 115.0 AND 121.50;
+
+-- 6. Usando a query a seguir, exiba apenas os valores de total do custo de substituição que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost), de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
+-- 	  SELECT rating, SUM(replacement_cost)
+--    FROM sakila.film
+-- 	  GROUP by rating;
+	SELECT rating, SUM(replacement_cost) as replacement_cost_by_rating
+	FROM sakila.film
+	GROUP by rating
+    HAVING replacement_cost_by_rating > 3950.50
+    ORDER BY replacement_cost_by_rating;
+
+
