312a49f284325ec7b350a80fc6c69941ceb73a47
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-31 18:41:48 -0300

    Ex06 - Steps 3 and 4

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex06-GroupByHaving.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Ex06-GroupByHaving.sql
@@ -11,5 +11,13 @@
 	GROUP BY store_id, `active`;
     
 -- 3. Monte uma query que exiba a média de duração de locação por classificação indicativa (rating) dos filmes cadastrados na tabela sakila.film. Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.
+	SELECT rating, AVG(rental_duration)
+    FROM sakila.film
+	GROUP BY rating
+    ORDER BY AVG(rental_duration) DESC;
 
 -- 4. Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de endereços registrados nele. Os resultados devem ser ordenados da maior quantidade para a menor.
+	SELECT district, count(*)
+    FROM sakila.address
+    GROUP BY district
+    ORDER BY count(*) desc;
