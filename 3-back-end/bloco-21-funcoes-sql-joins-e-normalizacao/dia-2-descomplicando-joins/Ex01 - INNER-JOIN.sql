625c559e22c38017591a50d513c056fa965aab0c
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-05-31 23:46:54 -0300

    Ex01 INNER JOIN - Steps 4 - 7

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/Ex01 - INNER-JOIN.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/Ex01 - INNER-JOIN.sql
@@ -22,13 +22,42 @@
     LIMIT 100;
 
 -- 4. Exiba o nome, email, id do endereço, endereço e distrito dos clientes que moram no distrito da California e que contêm "rene" em seus nomes. As informações podem ser encontradas nas tabelas address e customer.
-
+	SELECT c.first_name, c.email, c.address_id, a.address, a.district
+    FROM sakila.customer as c
+    INNER JOIN sakila.address as a
+    ON a.address_id = c.address_id
+    WHERE a.district LIKE '%CALIFORNIA%'
+    AND c.first_name LIKE '%RENE%';
 
 -- 5. Exiba o nome e a quantidade de endereços dos clientes cadastrados. Ordene seus resultados por nomes de forma decrescente. Exiba somente os clientes ativos. As informações podem ser encontradas na tabela address e customer.
-
+	SELECT c.first_name, count(a.address)
+    FROM sakila.customer AS c
+    INNER JOIN sakila.address AS a
+	ON a.address_id = c.address_id
+    WHERE c.`active` = 1
+    GROUP BY c.first_name
+    ORDER BY c.first_name DESC;
 
 -- 6. Monte uma query que exiba o nome, sobrenome e a média de valor (amount) paga aos funcionários no ano de 2006. Use as tabelas payment e staff. Os resultados devem estar agrupados pelo nome e sobrenome do funcionário.
-
+	SELECT s.first_name, s.last_name, avg(p.amount) as avg_payment_at_2006
+    FROM sakila.staff as s
+    INNER JOIN sakila.payment as p
+    ON s.staff_id = p.staff_id
+    WHERE YEAR(p.payment_date) = 2006
+    GROUP BY s.first_name;
 
 -- 7. Monte uma query que exiba o id do ator, nome, id do filme e título do filme, usando as tabelas actor, film_actor e film. Dica: você precisará fazer mais de um JOIN na mesma query.
-
+	SELECT a.actor_id, a.first_name, f.film_id, f.title
+    FROM sakila.actor AS a
+    INNER JOIN sakila.film_actor AS fa
+    INNER JOIN sakila.film AS f
+    ON a.actor_id = fa.actor_id
+    AND fa.film_id = f.film_id;
+    
+    -- or
+    SELECT a.actor_id, a.first_name, f.film_id, f.title
+    FROM sakila.actor AS a
+    INNER JOIN sakila.film_actor AS fa
+    ON a.actor_id = fa.actor_id
+    INNER JOIN sakila.film AS f
+    ON fa.film_id = f.film_id;
