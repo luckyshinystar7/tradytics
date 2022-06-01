cbc4a694b7d35c4d10d71287224a72108109b8d5
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-01 14:11:53 -0300

    Ex03 JOIN - Step 5

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/Ex03 - All-Together-JOIN.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/Ex03 - All-Together-JOIN.sql
@@ -37,3 +37,8 @@
 	ORDER BY t.name;
 
 -- 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
+	SELECT *
+	FROM Theater AS t
+	RIGHT JOIN Movies AS m 
+    ON t.id = m.theater_id
+	ORDER BY t.name;
