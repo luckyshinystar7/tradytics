025f8b2123a6f24bad3da983f418c756ba499a68
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-01 13:46:19 -0300

    adiciona pixar-db

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/Ex02 - SELF-JOIN.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/Ex02 - SELF-JOIN.sql
@@ -12,13 +12,28 @@
     ON f.MANAGER_ID = m.EMPLOYEE_ID
     WHERE f.DEPARTMENT_ID <> m.DEPARTMENT_ID;
     
+    -- OR
+    
+    SELECT 
+    f.EMPLOYEE_ID,
+    CONCAT(f.FIRST_NAME, ' ', f.LAST_NAME) AS EMPLOYEE_NAME,
+    f.MANAGER_ID,
+    f.DEPARTMENT_ID,
+    CONCAT(m.FIRST_NAME, ' ', m.LAST_NAME) AS MANAGER_NAME
+FROM
+    hr.employees AS f,
+    hr.employees AS m
+WHERE
+    f.MANAGER_ID = m.EMPLOYEE_ID
+        AND f.DEPARTMENT_ID <> m.DEPARTMENT_ID;
+    
 -- 2. Exiba o Nome e a quantidade de pessoas lideradas de cada pessoa gerente.
 	SELECT 
-		CONCAT(m.FIRST_NAME, ' ', m.LAST_NAME) AS `MANAGER_NAME`,
-		COUNT(f.EMPLOYEE_ID) AS TEAM_QUANTITY
-        #f.MANAGER_ID  
-    FROM hr.employees AS f
-    INNER JOIN hr.employees AS m
-    ON f.MANAGER_ID = m.EMPLOYEE_ID
-    GROUP BY m.EMPLOYEE_ID;
+    CONCAT(m.FIRST_NAME, ' ', m.LAST_NAME) AS `MANAGER_NAME`,
+    COUNT(f.EMPLOYEE_ID) AS TEAM_QUANTITY
+FROM
+    hr.employees AS f
+        INNER JOIN
+    hr.employees AS m ON f.MANAGER_ID = m.EMPLOYEE_ID
+GROUP BY m.EMPLOYEE_ID;
     
