a81f5cc0563337aa902d8da157ee773ff0c86ec3
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-02 18:17:37 -0300

    Exercicio 3 - Normalizacao, criacao de db, tabelas e relacoes - Step 1

---
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-3-transformando-ideias-em-um-modelo-de-banco-de-dados/Exercicio03 - CriandoDB-Tables-Relacoes.sql
@@ -0,0 +1,10 @@
+-- Exercício 3: 🚀 Monte uma query que:
+-- 	1. Crie um banco de dados chamado normalization;
+	CREATE DATABASE IF NOT EXISTS normalization;
+
+-- 	2. Crie todas as tabelas resultantes do exercício 2 (na 2ª Forma Normal);
+
+
+-- 	3. Popule todas as tabelas com os dados fornecidos nos exercícios.
+
+    
