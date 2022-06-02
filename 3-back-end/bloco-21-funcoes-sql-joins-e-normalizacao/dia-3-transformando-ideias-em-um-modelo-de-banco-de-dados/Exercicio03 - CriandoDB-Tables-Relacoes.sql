c88f17b1f11ec7cda5887832b9ae6bed10099559
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-02 18:30:02 -0300

    Exercicio 3 - Normalizacao, criacao de db, tabelas e relacoes - Step 2

--- 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-3-transformando-ideias-em-um-modelo-de-banco-de-dados/Exercicio03 - CriandoDB-Tables-Relacoes.sql
+++ 3-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-3-transformando-ideias-em-um-modelo-de-banco-de-dados/Exercicio03 - CriandoDB-Tables-Relacoes.sql
@@ -3,7 +3,28 @@
 	CREATE DATABASE IF NOT EXISTS normalization;
 
 -- 	2. Crie todas as tabelas resultantes do exercício 2 (na 2ª Forma Normal);
-
+	USE normalization;
+    
+    CREATE TABLE funcionario(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100),
+    email VARCHAR(50),
+    telefone CHAR(11),
+    data_cadastro TIMESTAMP
+    );
+    
+    CREATE TABLE setor(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    setor VARCHAR(50)
+    );
+    
+    CREATE TABLE funcionario_setor(
+	id_funcionario INT NOT NULL,
+    id_setor INT NOT NULL,
+    FOREIGN KEY (id_funcionario) REFERENCES funcionario(id),
+    FOREIGN KEY (id_setor) REFERENCES setor(id)
+    );
 
 -- 	3. Popule todas as tabelas com os dados fornecidos nos exercícios.
 
