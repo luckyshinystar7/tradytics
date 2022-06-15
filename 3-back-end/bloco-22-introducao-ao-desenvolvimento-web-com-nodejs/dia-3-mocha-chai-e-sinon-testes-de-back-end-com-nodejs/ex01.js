367eb711a698f73fd1520d997938e92ca5ce243b
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-15 14:39:24 -0300

    Finaliza Exercícios 2 e 3

--- 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/ex01.js
+++ 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/ex01.js
@@ -6,6 +6,7 @@
     1.3 Crie as asserções validando se os retornos de cada cenário têm o tipo e o valor esperado. */
 
 const defineNumber = (number) => {
+  if (isNaN(number)) return 'o valor deve ser um número';
   if (number > 0 ) return 'positivo';
   if (number < 0 ) return 'negativo';
   if (number === 0 ) return 'neutro';
@@ -16,3 +17,12 @@ const defineNumber = (number) => {
 // console.log(defineNumber(-10));
 
 module.exports = defineNumber;
+
+/*  Exercício 2: Implemente a função apresentada no exercício 1, garantindo que ela vai passar em todos os testes que você escreveu.
+    ⚠️ Lembre-se de adicionar o script de test no package.json e de instalar as dependências.
+
+    Exercício 3: Adicione à função do exercício 1, um tratamento para casos em que o parâmetro informado não seja do tipo Number.
+    Adicione o cenário em seu arquivo de testes, passando um valor de tipo diferente a Number para a função;
+
+    Adicione uma asserção para esperar que o valor retornado para esse caso seja igual uma string "o valor deve ser um número";
+    Implemente em sua função tal validação para que o teste passe. */
--- 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/ex01.test.js
+++ 3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/ex01.test.js
@@ -5,6 +5,15 @@
     1.2 Descreva todos os testes que serão feitos utilizando its;
     1.3 Crie as asserções validando se os retornos de cada cenário têm o tipo e o valor esperado. */
 
+/* Exercício 2: Implemente a função apresentada no exercício 1, garantindo que ela vai passar em todos os testes que você escreveu.
+    ⚠️ Lembre-se de adicionar o script de test no package.json e de instalar as dependências.
+
+   Exercício 3: Adicione à função do exercício 1, um tratamento para casos em que o parâmetro informado não seja do tipo Number.
+    Adicione o cenário em seu arquivo de testes, passando um valor de tipo diferente a Number para a função;
+
+    Adicione uma asserção para esperar que o valor retornado para esse caso seja igual uma string "o valor deve ser um número";
+    Implemente em sua função tal validação para que o teste passe. */
+
 const { expect } = require('chai');
 const defineNumber = require('../ex01');
 
@@ -44,4 +53,17 @@ describe('1. Testa a função', () => {
       expect(result).to.be.equal('neutro');
     })
   })
+
+  describe('Quando inserido um valor não numérico', () => {
+    it('1.7 retorna uma string', () => {
+      const result = defineNumber('d');
+      expect(result).to.be.a('string');
+    })
+
+    it('1.8 retorna a string "neutro"', () => {
+      const result = defineNumber('d');
+      expect(result).to.be.equal('o valor deve ser um número');
+    })
+  })
 })
+
