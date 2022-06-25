31671602dc49bc65f4aac60669ac35498bca49cf
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-25 11:43:51 -0300

    Finaliza Exercício de Fixação Ex 1.1 conforme EnunciadosExercicios.js

---
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/EnunciadosExercicios.js
@@ -0,0 +1,10 @@
+// EXERCÍCIOS DE FIXAÇÃO
+/*
+Ex 01
+
+Depois de criar a tabela Books no banco de dados model_example, faça as seguintes implementações.
+1.1 - Crie um modelo Book e defina o método getAll para retornar a lista de todos os livros;
+1.2 - Crie uma rota /books para trazer a lista de todos os livros;
+1.3 - Crie um método getByAuthorId no modelo Book, para retornar apenas livros associados com um determinado author_id. Altere o middleware da rota books criado no passo  para receber uma query string com a chave author_id, e retornar apenas os livros associados.
+
+*/
---
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Book.js
@@ -0,0 +1,14 @@
+
+const connection = require('./connection');
+
+// 1.1 - Crie um modelo Book e defina o método getAll para retornar a lista de todos os livros;
+const getAll = async () => {
+  const [books] = await connection.execute(
+    'SELECT title, author_id FROM model_example.books;',
+  );
+  return books.map(serialize).map(getNewAuthor);
+};
+
+module.exports = {
+  getAll,
+};
