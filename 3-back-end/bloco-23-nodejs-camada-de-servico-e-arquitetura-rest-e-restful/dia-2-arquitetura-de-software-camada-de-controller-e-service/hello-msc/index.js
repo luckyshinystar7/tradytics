0d609f8ff7baef1cc48f3935c90f4d5853f150a6
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-27 22:45:18 -0300

    exemplo MSC

---
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/index.js
@@ -0,0 +1,21 @@
+// index.js
+
+const express = require('express');
+
+const Author = require('./controllers/Authors');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/authors', Author.getAll);
+
+app.get('/authors/:id', Author.findById);
+
+app.post('/authors', Author.createAuthor);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
