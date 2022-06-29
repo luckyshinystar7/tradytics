52d9d98162b5eab5e34338bbbdbcd4c1b21d49db
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-29 13:30:16 -0300

    adiciona validação que retorna erro caso não exista author com o id fornecido

--- 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/services/Author.js
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/services/Author.js
@@ -2,45 +2,28 @@
 
 const Author = require('../models/Author');
 
-const getNewAuthor = (authorData) => {
-  const { id, firstName, middleName, lastName } = authorData;
-
-  const fullName = [firstName, middleName, lastName]
-    .filter((name) => name)
-    .join(' ');
-
-  return {
-    id,
-    firstName,
-    middleName,
-    lastName,
-    name: fullName,
-  };
-};
-
-const isValid = (firstName, middleName, lastName) => {
-  if (!firstName || typeof firstName !== 'string') return false;
-  if (!lastName || typeof lastName !== 'string') return false;
-  if (middleName && typeof middleName !== 'string') return false;
-
-  return true;
-};
-
-const getAll = async () => {
-  const authors = await Author.getAll();
-
-  return authors.map(getNewAuthor);
-};
+const getAll = async () => Author.getAll();
 
 const findById = async (id) => {
   const author = await Author.findById(id);
 
-  if (!author) return null;
+  if (!author) {
+    return {
+      error: {
+        code: 'notFound',
+        message: `Não foi possível encontrar uma pessoa autora com o id ${id}`,
+      },
+    };
+  }
 
-  return getNewAuthor(author);
+  return author;
 };
+
+const createAuthor = async (firstName, middleName, lastName) =>
+  Author.createAuthor(firstName, middleName, lastName);
 
 module.exports = {
   getAll,
   findById,
+  createAuthor,
 };
