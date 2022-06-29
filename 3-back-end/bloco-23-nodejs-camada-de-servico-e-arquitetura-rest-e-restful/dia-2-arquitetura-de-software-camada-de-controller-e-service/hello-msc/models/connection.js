05afae71e466f3194136d1f12c8a1b596a54e6c7
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-06-29 16:52:24 -0300

    adiciona user e password do MYSQL em variaveis de ambiente

--- 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/models/connection.js
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/models/connection.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  user: 'root',
-  password: 'thatsMySqlPW',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB_NAME,
   });
 
