a911a70fad5bbb6481988d141d2c646f89b6cb7a
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-07-04 16:35:52 -0300

    Cria camada services e transfere functions de validação para essa camada

--- 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/23.2-exercicios/models/User.js
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/23.2-exercicios/models/User.js
@@ -1,65 +1,5 @@
 const connection = require('./connection');
 
-const validateFirstName = (firstName) => {
-  if (!firstName) {
-    return {
-      error: true,
-      message: 'O campo firstName é obrigatório',
-    };
-  }
-  return { error: false };
-};
-
-const validateLastName = (lastName) => {
-  if (!lastName) {
-    return {
-      error: true,
-      message: 'O campo lastName é obrigatório',
-    };
-  }
-  return { error: false };
-};
-
-const validateEmail = (email) => {
-  if (!email) {
-    return {
-      error: true,
-      message: 'O campo "email" é obrigatório e deve possuir o seguinte formato: "email@email.com',
-    };
-  }
-  return { error: false };
-};
-
-const validatePassword = (password) => {
-  if (typeof password !== 'string' || password.length < 6) {
-    return {
-      error: true,
-      message: 'O campo "password" deve ter pelo menos 6 caracteres',
-    };
-  }
-  return { error: false };
-};
-
-const validate = async ({ firstName, lastName, email, password }) => {
-  // fazer validações (retornar um objeto com 2 chaves
-  // {
-  //   "error": true,
-  //   "message": "O campo 'password' deve ter pelo menos 6 caracteres"
-  // })
-  if (validateFirstName(firstName).error) return validateFirstName(firstName);
-
-  if (validateLastName(lastName).error) return validateLastName(lastName);
-
-  if (validateEmail(email).error) return validateEmail(email);
-
-  if (validatePassword(password).error) return validatePassword(password);
-
-  return {
-    error: false,
-    message: null,
-  };
-};
-
 async function insert({ firstName, lastName, email, password }) {
   const insertQuery = 'INSERT INTO users_crud.users '
     + '(`first_name`, `last_name`, `email`, `password`) VALUES (?,?,?,?);';
@@ -93,7 +33,6 @@ const edit = async (id, { firstName, lastName, email, password }) => {
 };
 
 module.exports = {
-  validate,
   insert,
   get,
   getById,
