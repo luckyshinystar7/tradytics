557d1c21fc056584245149c751f198164a8192d8
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-07-05 14:05:12 -0300

    adiciona camada models e nela function getAll

---
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/models/cepModels.js
@@ -0,0 +1,14 @@
+const db = require('./db');
+
+// busca todos os dados do banco
+const getAll = async () => {
+  const [ceps] = await db.query(`
+  SELECT * 
+  FROM ceps;
+  `);
+  return ceps;
+};
+
+module.exports({
+  getAll,
+});
