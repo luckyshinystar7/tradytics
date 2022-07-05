512785118c268efb955005c5cd7e8e335d154c27
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-07-05 13:41:39 -0300

    finaliza setup inicial com variaveis de ambiente

---
+++ 3-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`rodando na porta ${PORT}`);
+});
