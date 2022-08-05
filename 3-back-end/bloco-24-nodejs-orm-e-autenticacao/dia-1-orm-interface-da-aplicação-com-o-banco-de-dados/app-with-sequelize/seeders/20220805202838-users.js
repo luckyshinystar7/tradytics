b39e9b0ebdfd4b840efca75f74fbb77068359ef8
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-08-05 17:37:42 -0300

    cria função responsável por popular/reverter os dados no db

--- 3-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-1-orm-interface-da-aplicação-com-o-banco-de-dados/app-with-sequelize/seeders/20220805202838-users.js
+++ 3-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-1-orm-interface-da-aplicação-com-o-banco-de-dados/app-with-sequelize/seeders/20220805202838-users.js
@@ -1,24 +1,22 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users',
+    [
+      {
+        fullName: 'Leonardo',
+        email: 'leo@test.com',
+        // usamos a função CURRENT_TIMESTAMP do SQL para salvar a data e hora atual nos campos `createdAt` e `updatedAt`
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        fullName: 'Eduardo',
+        email: 'edu@test.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ], {}),
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: async (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
 };
