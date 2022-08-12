711c4a58b54a1bea8f390f62cc8df735582cc34e
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-08-12 16:54:03 -0300

    adiciona exemplos de managed e unmanaged transactions

--- 3-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-2-orm-associations/exercicio-fixacao/index.js
+++ 3-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-2-orm-associations/exercicio-fixacao/index.js
@@ -1,8 +1,15 @@
 // index.js
 const express = require('express');
+const Sequelize = require('sequelize'); // necessário para transações
 const { Address, Employee, User, Book } = require('./models');
 
+const config = require('./config/config'); // necessário para transações
+
 const app = express();
+
+app.use(express.json());
+
+const sequelize = new Sequelize(config.development); // necessário para transações
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -75,6 +82,83 @@ app.get('/usersbooks/:id', async (req, res) => {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   };
+});
+
+// FORMA NORMAL 
+// app.post('/employees', async (req, res) => {
+//   try {
+//     const { firstName, lastName, age, city, street, number } = req.body;
+
+//     const employee = await Employee.create({ firstName, lastName, age });
+
+//     await Address.create({ city, street, number, employeeId: employee.id });
+
+//     return res.status(201).json({ message: 'Cadastrado com sucesso' });
+//   } catch (e) {
+//     console.log(e.message);
+//     res.status(500).json({ message: 'Algo deu errado' });
+//   }
+// });
+
+// FORMA USANDO MANAGED MICRO TRANSACTION
+// app.post('/employees', async (req, res) => {
+//   try {
+//     const { firstName, lastName, age, city, street, number } = req.body;
+
+//     const result = await sequelize.transaction(async (t) => {
+//       const employee = await Employee.create({
+//         firstName, lastName, age
+//       }, { transaction: t });
+
+//       await Address.create({
+//         city, street, number, employeeId: employee.id
+//       }, { transaction: t });
+
+//       return res.status(201).json({ message: 'Cadastrado com sucesso' });
+//     });
+
+    // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+    // não sendo necessário finalizar a transação manualmente.
+    // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+  // } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    // console.log(e.message);
+    // res.status(500).json({ message: 'Algo deu errado' });
+  // }
+// });
+
+// FORMA USANDO UNMANAGED MICRO TRANSACTION
+app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    // Depois executamos as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
