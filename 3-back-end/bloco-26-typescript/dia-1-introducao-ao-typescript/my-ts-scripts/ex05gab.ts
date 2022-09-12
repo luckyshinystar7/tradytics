1b8153240d1abb7e344d655f2baec77857d98ea5
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-09-12 16:38:46 -0300

    gabarito ex5

---
+++ 3-back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/ex05gab.ts
@@ -0,0 +1,9 @@
+const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
+
+function convert(value: number, fromUnit: string, toUnit: string): number {
+    const fromIndex = units.indexOf(fromUnit); // pegamos o index da unidade base no array
+    const toIndex = units.indexOf(toUnit); // pegamos o index da unidade para a conversão
+    const exponent = (toIndex - fromIndex); // calculamos o expoente a partir da diferença dos index
+
+    return value * Math.pow(10, exponent);
+}
