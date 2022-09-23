22deeea78eaeca317c3d385145e34c1c0375b15e
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-09-23 19:57:05 -0300

    Exemplo: criando uma classe com atributos e metodos estáticos, que remetem apenas a classe e nao aos objetos instanciados

--- 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/fixacao/exemplo02-abstract.ts
+++ 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/fixacao/exemplo02-abstract.ts
@@ -2,28 +2,28 @@
 Dicionário en-pt:
 - fish: peixe
 */
-abstract class Animal {
+abstract class Animal2 {
   constructor(public name: string) { }
   abstract move(): void
 }
-class Bird extends Animal {
+class Bird2 extends Animal2 {
   move() { console.log(`${this.name} está voando.`); }
 }
-class Mammal extends Animal {
+class Mammal2 extends Animal2 {
   move() { console.log(`${this.name} está andando.`); }
 }
-class Fish extends Animal {
+class Fish2 extends Animal2 {
   move() { console.log(`${this.name} está nadando.`); }
 }
-const a = new Fish('Tubarão');
-const b = new Bird('Papagaio');
-const m = new Mammal('Tatu');
-const myMove = (animal: Animal) => {
+const a2 = new Fish2('Tubarão');
+const b2 = new Bird2('Papagaio');
+const m2 = new Mammal2('Tatu');
+const myMove2 = (animal: Animal2) => {
   animal.move();
 }
-myMove(a);
-myMove(b);
-myMove(m);
+myMove2(a);
+myMove2(b);
+myMove2(m);
 /*
 Saída:
 Tubarão está nadando.
---
+++ 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-3-polimorfismo/fixacao/exemplo03-static.ts
@@ -0,0 +1,28 @@
+/*
+Dicionário en-pt:
+- employee: pessoa empregada/funcionária
+- static: estático
+*/
+class Employee {
+  private static employeeCount = 0
+  constructor(public name: string) {
+    Employee.addEmployee();
+  }
+  private static addEmployee() {
+    this.employeeCount += 1;
+  }
+  static get employees() {
+    return this.employeeCount;
+  }
+}
+console.log(Employee.employees);
+const e1 = new Employee('Ronald');
+console.log(Employee.employees);
+const e2 = new Employee('Cíntia');
+console.log(Employee.employees);
+/*
+Saída:
+0
+1
+2
+*/
