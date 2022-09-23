95d13d758bb24c358b7fd226b4d1f7166e705cb4
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-09-23 18:54:02 -0300

    Inicia exercicio 04

---
+++ 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercicio/Subject.ts
@@ -0,0 +1,14 @@
+/* 
+  Exercício 4: Crie uma classe cujos objetos representem uma disciplina lecionada na escola.
+
+  `Class`: Subject
+`Attributes`:
+    - name: nome da disciplina
+`Methods`:
+    - Getters/Setters
+    - constructor: deve receber como parâmetro nome
+`Validations`:
+    - O nome tem que possuir no mínimo 3 caracteres
+*/
+
+
