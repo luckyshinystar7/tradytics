844db07b577dbe13be21c58af9c4498d883a7999
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-09-23 18:37:20 -0300

    Finaliza Exercicio 02

--- 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercicio/Student.ts
+++ 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercicio/Student.ts
@@ -20,3 +20,54 @@
     - A pessoa estudante deve possuir no máximo 4 notas de provas
     - A pessoa estudante deve possuir no máximo 2 notas de trabalhos
 */
+
+import Person from "./Person"
+
+export default class Student extends Person {
+  private _enrollment:string;
+  private _examsGrades:number[];
+  private _worksGrades:number[];
+
+  constructor(name:string, birthDate:Date) {
+    super(name, birthDate);
+    this._enrollment = this.generateEnrollment() 
+  }
+
+  get enrollment():string {
+    return this._enrollment;
+  }
+
+  get examsGrades():number[] {
+    return [...this._examsGrades];
+  }
+
+  get worksGrades():number[] {
+    return [...this._worksGrades];
+  }
+
+  set examsGrades(grades:number[]) {
+    this.validateExamGrades(grades);
+    this.worksGrades = [...grades];
+  }
+
+  set worksGrades(grades:number[]) {
+    this.validateWorkGrades(grades);
+    this.worksGrades = [...grades];
+  }
+
+  private generateEnrollment():string {
+    return Number(Math.random()*100000).toFixed(0); // gera um ID aleatório de 5 digitos
+  }
+
+  private validateExamGrades(grades:number[]):void {
+    if (grades.length > 4) {
+      throw new Error ('Limite de provas excedido')
+    }
+  }
+
+  private validateWorkGrades(grades:number[]):void {
+    if (grades.length > 2) {
+      throw new Error ('Limite de trabalhos excedido')
+    }
+  }
+}
