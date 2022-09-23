13deefbbca8e6ee0845507e06d0de0299a63e404
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-09-23 18:58:03 -0300

    Finaliza exercicio 04

--- 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercicio/Subject.ts
+++ 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercicio/Subject.ts
@@ -11,4 +11,26 @@
     - O nome tem que possuir no mínimo 3 caracteres
 */
 
+export default class Subject {
+  private _name:string;
 
+  constructor(name:string) {
+    this.validateName(name);
+    this._name = name;
+  }
+
+  get name():string {
+    return this._name;
+  }
+
+  set name(name:string) {
+    this.validateName(name);
+    this._name = name;
+  }
+
+  private validateName(name:string):void {
+    if (name.length < 3) {
+      throw new Error ('Nome deve possuir no mínimo 3 caracteres');
+    }
+  }
+}
