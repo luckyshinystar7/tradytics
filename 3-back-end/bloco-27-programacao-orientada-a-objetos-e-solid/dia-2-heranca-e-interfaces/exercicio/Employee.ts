344a98999f488e54e204fbdff812fd8969ad3300
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-09-23 18:51:13 -0300

    Inicia exercicio 03

---
+++ 3-back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-2-heranca-e-interfaces/exercicio/Employee.ts
@@ -0,0 +1,12 @@
+/*
+  Exercício 3: Crie uma interface que representará uma pessoa funcionária.
+
+`Interface`: Employee
+`Attributes`:
+    - registration: número do registro
+    - salary: valor do salário
+    - admissionDate: data de admissão
+`Methods`:
+    - generateRegistration: retorna uma string única gerada como registro
+*/
+
