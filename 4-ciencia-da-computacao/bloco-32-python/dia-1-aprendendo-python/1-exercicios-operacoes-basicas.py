9dbff494a9efd16c8986bc5ec31f77d74397c636
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-11-25 20:55:40 -0300

    Finaliza exercício Ex02

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/1-exercicios-operacoes-basicas.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/1-exercicios-operacoes-basicas.py
@@ -12,6 +12,10 @@ a ** b
 a % b
 
 # Exercício 2: Declare e inicialize uma variável: hours = 6. Quantos minutos têm em 6 horas? E quantos segundos? Declare e inicialize variáveis minutes e seconds que recebem os respectivos resultados das contas. Depois, imprima cada uma delas.
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+print(hours, minutes, seconds)
 
 # Exercício 3: Teste e verifique o que acontece se você colocar um ponto e vírgula no final de uma instrução em Python.
 
