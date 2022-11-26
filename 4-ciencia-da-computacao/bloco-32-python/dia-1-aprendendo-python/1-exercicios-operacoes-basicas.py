13fc867b9bb63a10603fd8a1258872ccf7247b31
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-11-25 21:12:54 -0300

    Finaliza exercício Ex04

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/1-exercicios-operacoes-basicas.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/1-exercicios-operacoes-basicas.py
@@ -20,4 +20,12 @@ print(hours, minutes, seconds)
 # Exercício 3: Teste e verifique o que acontece se você colocar um ponto e vírgula no final de uma instrução em Python.
 3+2; # nada aconteceu, o comando rodou normalmente
 
-# Exercício 4: Suponha que o preço de capa de um livro seja R$ 24,20, mas as livrarias recebem um desconto de 40%. O transporte custa 3,00 para o primeiro exemplar e 75 centavos para cada exemplar adicional. Qual é o custo total de atacado para 60 cópias? Escreva uma expressão que receba o custo total e a imprima.
+# Exercício 4: Suponha que o preço de capa de um livro seja R$ 24,20, mas as livrarias recebem um desconto de 40%. O transporte custa 3,00 para o primeiro exemplar e 75 centavos para cada exemplar adicional. 
+# Qual é o custo total de atacado para 60 cópias? Escreva uma expressão que receba o custo total e a imprima.
+preco_livro = 24.20
+preco_livro_desc_40 = preco_livro * 0.6
+preco_60_copias_desc = 60*preco_livro_desc_40
+frete_60_copias = 3 + 59*0.75
+preco_60_copias_com_frete = preco_60_copias_desc + frete_60_copias
+
+print(preco_60_copias_com_frete)
