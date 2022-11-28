2cab5f4a60cd2fe977c26a183988deb9578eee02
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-11-28 11:42:46 -0300

    Atualiza resumo de comandos em python
    Adiciona conteúdo sobre dicionários no arquivo de resumo

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/comandos-python.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/comandos-python.py
@@ -18,12 +18,14 @@ fruits.index("maçã")  # retorna o índice onde a fruta está localizada, neste
 
 fruits.sort()  # ordena a lista de frutas
 
+
 # SOBRE TUPLAS (tuple)
 # São similares a listas, porém não podem ser modificados durante a execução do programa.
 
 user = ("Will", "Marcondes", 42)  # elementos são definidos separados por vírgula, envolvidos por parênteses
 
 user[0]  # acesso também por índices
+
 
 # SOBRE CONJUNTOS (set)
 # Um conjunto é uma coleção de elementos únicos e não ordenados. Conjuntos implementam operações de união, intersecção e outras.
@@ -40,6 +42,7 @@ permissions.intersection({"user", "member"})  # retorna um conjunto resultante d
 
 permissions.difference({"user"})  # retorna a diferença entre os dois conjuntos
 
+
 # SOBRE CONJUNTOS IMUTÁVEIS (frozenset)
 # É uma variação do set, porém imutável, ou seja, seus elementos não podem ser modificados durante a execução do programa.
 
@@ -50,3 +53,19 @@ permissions.union({"user"})  # novos conjuntos imutáveis podem ser criados à p
 permissions.intersection({"user", "member"})  # retorna um conjunto resultante da intersecção dos conjuntos
 
 permissions.difference({"user"})  # retorna a diferença entre os dois conjuntos
+
+
+# SOBRE DICIONÁRIOS (dict)
+# Estrutura que associa uma chave a um determinado valor. É a representação do tão famoso objeto que utilizamos em JavaScript.
+
+people_by_id = {1: "Maria", 2: "Fernanda", 3: "Felipe"}  # elementos no formato "chave: valor" separados por vírgula, envolvidos por chaves
+
+people_by_name = {"Maria": 1, "Fernanda": 2, "Felipe": 3}  # outro exemplo, dessa vez usando strings como chaves. As aspas são necessárias para que o Python não ache que `Maria`, `Fernanda` e `Felipe` sejam variáveis.
+
+# elementos são acessados por suas chaves
+people_by_id[1]  # saída: Maria
+
+# elementos podem ser removidos com a palavra chave del
+del people_by_id[1]
+people_by_id.items()  # dict_items([(1, "Maria"), (2, "Fernanda"), (3, "Felipe")])
+# um conjunto é retornado com tuplas contendo chaves e valores
