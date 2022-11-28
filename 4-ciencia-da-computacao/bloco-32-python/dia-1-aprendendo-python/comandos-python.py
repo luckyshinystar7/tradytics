35322688e86c6f9e6fa0744b53528c5c311e4a5b
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-11-28 11:40:35 -0300

    Atualiza resumo de comandos em python
    Adiciona conteúdo sobre conjuntos imutáveis no arquivo de resumo

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/comandos-python.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/comandos-python.py
@@ -40,4 +40,13 @@ permissions.intersection({"user", "member"})  # retorna um conjunto resultante d
 
 permissions.difference({"user"})  # retorna a diferença entre os dois conjuntos
 
+# SOBRE CONJUNTOS IMUTÁVEIS (frozenset)
+# É uma variação do set, porém imutável, ou seja, seus elementos não podem ser modificados durante a execução do programa.
 
+permissions = frozenset(["member", "group"])  # assim como o set, qualquer estrutura iterável pode ser utilizada para criar um frozenset
+
+permissions.union({"user"})  # novos conjuntos imutáveis podem ser criados à partir do original, mas o mesmo não pode ser modificado
+
+permissions.intersection({"user", "member"})  # retorna um conjunto resultante da intersecção dos conjuntos
+
+permissions.difference({"user"})  # retorna a diferença entre os dois conjuntos
