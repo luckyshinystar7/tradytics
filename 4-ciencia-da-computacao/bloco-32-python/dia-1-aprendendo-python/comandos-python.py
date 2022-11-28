1ada43ca85ce51ca7b3751eecfc4b623d87f1adb
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-11-28 11:37:40 -0300

    Atualiza resumo de comandos em python
    Adiciona conteúdo sobre conjuntos no arquivo de resumo

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/comandos-python.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/comandos-python.py
@@ -18,9 +18,26 @@ fruits.index("maçã")  # retorna o índice onde a fruta está localizada, neste
 
 fruits.sort()  # ordena a lista de frutas
 
-# SOBRE TUPLAS
+# SOBRE TUPLAS (tuple)
 # São similares a listas, porém não podem ser modificados durante a execução do programa.
 
 user = ("Will", "Marcondes", 42)  # elementos são definidos separados por vírgula, envolvidos por parênteses
 
 user[0]  # acesso também por índices
+
+# SOBRE CONJUNTOS (set)
+# Um conjunto é uma coleção de elementos únicos e não ordenados. Conjuntos implementam operações de união, intersecção e outras.
+
+permissions = {"member", "group"}  # elementos separados por vírgula, envolvidos por chaves
+
+permissions.add("root")  # adiciona um novo elemento ao conjunto
+
+permissions.add("member")  # como o elemento já existe, nenhum novo item é adicionado ao conjunto
+
+permissions.union({"user"})  # retorna um conjunto resultado da união
+
+permissions.intersection({"user", "member"})  # retorna um conjunto resultante da intersecção dos conjuntos
+
+permissions.difference({"user"})  # retorna a diferença entre os dois conjuntos
+
+
