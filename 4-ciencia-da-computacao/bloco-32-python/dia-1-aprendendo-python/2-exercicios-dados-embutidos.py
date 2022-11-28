7646014a9dfbc30d1d6839b9bb506c295f9b59cf
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-11-28 11:39:12 -0300

    Finaliza exercício Ex07

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/2-exercicios-dados-embutidos.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/2-exercicios-dados-embutidos.py
@@ -9,3 +9,8 @@ trybe_course.append("Ciência da Computação")
 
 # Exercício 6: Acesse e altere o primeiro elemento da lista para “Fundamentos”.
 trybe_course[0] = "Fundamentos"
+
+# Exercício 7: Um conjunto ou set pode ser inicializado utilizando-se também o método set(). Inicialize uma variável com essa função var = set() e adicione seu nome ao conjunto utilizando um dos métodos vistos. Depois, imprima a variável e confira se o retorno é: {‘seu_nome’}.
+var = set()
+var.add("Elthon")
+var
