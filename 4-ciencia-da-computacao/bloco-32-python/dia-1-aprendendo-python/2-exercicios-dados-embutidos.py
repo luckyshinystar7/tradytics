ee8bdadf4e6dcebad5b5a4e587528b495ae5deab
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-11-28 11:27:30 -0300

    Finaliza exercício Ex05

---
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/2-exercicios-dados-embutidos.py
@@ -0,0 +1,10 @@
+#   EXERCICIOS SOBRE DADOS EMBUTIDOS
+
+# LISTAS
+# Copie a lista abaixo para realizarmos os exercícios de fixação 5 e 6:
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+
+# Exercício 5: Adicione o elemento “Ciência da Computação” à lista.
+trybe_course.append("Ciência da Computação")
+
+# Exercício 6: Acesse e altere o primeiro elemento da lista para “Fundamentos”.
