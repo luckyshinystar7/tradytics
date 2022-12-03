eccbf1f3cbd848dd92edce5b20ec7e63f8431e16
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-12-02 23:03:32 -0300

    Adiciona FOR no resumo Python
    Adiciona conteúdo sobre a estrutura de repetição FOR no arquivo de resumo

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/comandos-python.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/comandos-python.py
@@ -90,3 +90,24 @@ list(range(1, 11, 2))  # saída: [1, 3, 5, 7, 9]
 
 # podemos utilizar valores negativos para as entradas também
 list(range(10, 0, -1))  # saída: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+# EXEMPLOS ESTRUTURAS DE REPETIÇÃO
+# FOR
+
+restaurants = [
+    {"name": "Restaurante A", "nota": 4.5},
+    {"name": "Restaurante B", "nota": 3.0},
+    {"name": "Restaurante C", "nota": 4.2},
+    {"name": "Restaurante D", "nota": 2.3},
+]
+
+filtered_restaurants = []
+min_rating = 3.0
+for restaurant in restaurants:
+    if restaurant["nota"] > min_rating:
+        filtered_restaurants.append(restaurant)
+print(filtered_restaurants)  # imprime a lista de restaurantes, sem o B e D
+
+# utilizando range em um for
+for index in range(5):
+    print(index)
