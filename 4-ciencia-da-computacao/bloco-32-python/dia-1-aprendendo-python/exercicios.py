d99f0852e2198766e019b57285402e6f9dff326f
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-12-27 21:31:41 -0300

    Finaliza exercicio 03

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/exercicios.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/exercicios.py
@@ -18,4 +18,16 @@ def media(a):
     return med
 
 
-media([1, 2, 4, 3, 7, 10, 2, 12, 45, 32, 5, 67])
+# media([1, 2, 4, 3, 7, 10, 2, 12, 45, 32, 5, 67])
+
+
+# Exercício 3: Faça um programa que, dado um valor n qualquer, tal que n > 1,
+# imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+def square(n):
+    i = 0
+    while i < n:
+        print(n*'*')
+        i = i + 1
+
+
+square(2)
