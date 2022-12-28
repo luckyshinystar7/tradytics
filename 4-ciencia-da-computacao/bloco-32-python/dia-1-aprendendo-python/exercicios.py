37713f41e334bc8ab679b56653dcdfe4fdfb3f61
luckyshinystar7 <luckyshinystar7@gmail.com>
2022-12-27 21:55:24 -0300

    Inicia exercicio 05

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/exercicios.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/exercicios.py
@@ -46,4 +46,10 @@ def maior_nome(lista):
     return nome
 
 
-maior_nome(["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"])
+# maior_nome(["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"])
+
+# Exercício 5: Considere que a cobertura da tinta é de 1 litro para cada
+# 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que
+# custam R$ 80,00. Crie uma função que retorne dois valores em uma tupla
+# contendo a quantidade de latas de tinta a serem compradas
+# e o preço total a partir do tamanho de uma parede (em m²).
