172a30d50cec0edc3b19defa28deeee3ba865f78
luckyshinystar7 <luckyshinystar7@gmail.com>
2023-01-02 12:32:30 -0300

    Reescreve nome de arquivos utilizando o padrao snake case

--- 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/1_exercicios_operacoes_basicas.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-1-aprendendo-python/1_exercicios_operacoes_basicas.py
@@ -18,7 +18,8 @@ seconds = minutes * 60
 print(hours, minutes, seconds)
 
 # Exercício 3: Teste e verifique o que acontece se você colocar um ponto e vírgula no final de uma instrução em Python.
-3 + 2; # nada aconteceu, o comando rodou normalmente acusando porém erro de linter
+3 + 2
+# nada aconteceu, o comando rodou normalmente acusando porém erro de linter
 
 # Exercício 4: Suponha que o preço de capa de um livro seja R$ 24,20, mas as livrarias recebem um desconto de 40%. O transporte custa 3,00 para o primeiro exemplar e 75 centavos para cada exemplar adicional.
 # Qual é o custo total de atacado para 60 cópias? Escreva uma expressão que receba o custo total e a imprima.
