a8edaac6ecfef0b2f635e2d5d3f40dd8d85e3414
luckyshinystar7 <luckyshinystar7@gmail.com>
2023-01-04 22:23:32 -0300

    Continua exercício de fixação 3
    Cria código para geração do arquivo txt necessário para a realização do exercício

--- 4-ciencia-da-computacao/bloco-32-python/dia-2-entrada-e-saida-de-dados/ex3_fixacao_testes.py
+++ 4-ciencia-da-computacao/bloco-32-python/dia-2-entrada-e-saida-de-dados/ex3_fixacao_testes.py
@@ -14,3 +14,18 @@
 # Ana 10
 # Maria 9
 # Miguel 5
+
+# 🦜 Dica: A função split pode ser utilizada para dividir o
+# nome em uma linha. Ex: line.split() -> ["Marcos", "10"].
+
+# Primeiro vamos criar o arquivo:
+ex3_file = open("ex3_file.txt", mode="w")
+LINES = ["Marcos 10\n", "Felipe 4\n", "José 6\n", "Ana 10\n", "Maria 9\n"]
+ex3_file.writelines(LINES)
+ex3_file.write("Miguel 5")
+ex3_file.close()
+
+# Agora vamos acessar o arquivo conforme enunciado
+names_and_grades = open("ex3_file.txt", mode="r")
+print(names_and_grades)
+names_and_grades.close()
