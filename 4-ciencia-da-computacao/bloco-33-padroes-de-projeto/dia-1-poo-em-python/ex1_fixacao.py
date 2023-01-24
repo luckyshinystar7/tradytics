ba6ccbb874670ac7ad6bcd5962ae6125bb9f5e00
luckyshinystar7 <luckyshinystar7@gmail.com>
2023-01-24 20:56:06 -0300

    Finaliza exercicio 1.1 de fixacao

---
+++ 4-ciencia-da-computacao/bloco-33-padroes-de-projeto/dia-1-poo-em-python/ex1_fixacao.py
@@ -0,0 +1,38 @@
+# Exercício de fixação 1
+
+# Além do liquidificador, podemos modelar inúmeros eletrodomésticos,
+# tais como: ventilador, batedeira, secador, máquina de lavar e etc.
+
+# 1.1 - Para fixar crie uma objeto da classe Ventilador,
+# semelhante ao exemplo do liquidificador.
+
+# 1.2 - Em seguida faça com que a pessoa tenha um ventilador como atributo
+# através da composição.
+
+# 1.3 - Crie também um método comprar_ventilador.
+
+# 1.4 - Faça com que o print (__str__) de Pessoa informe se sua instância
+# possui ou não um ventilador.
+
+# 1.1
+class Ventilador:
+    def __init__(self, cor, potencia, preco,  tensao):
+        self.cor = cor
+        self.potencia = potencia
+        self.tensao = tensao
+        self.preco = preco
+
+    def cor(self):
+        return self.cor
+
+
+class Pessoa:
+    def __init__(self, nome, saldo_na_conta):
+        self.nome = nome
+        self.saldo_na_conta = saldo_na_conta
+        self.liquidificador = None
+
+    def comprar_liquidificador(self, liquidificador):
+        if liquidificador.preco <= self.saldo_na_conta:
+            self.saldo_na_conta -= liquidificador.preco
+            self.liquidificador = liquidificador
