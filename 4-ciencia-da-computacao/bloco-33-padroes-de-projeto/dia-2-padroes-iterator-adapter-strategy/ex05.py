31057c797dc54c879d860f3f04f6e871c3a754d6
luckyshinystar7 <luckyshinystar7@gmail.com>
2023-01-30 12:55:43 -0300

    Adiciona resolução de exercício 5

---
+++ 4-ciencia-da-computacao/bloco-33-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/ex05.py
@@ -0,0 +1,34 @@
+# Exercício 5: Você tem a implementação de uma classe capaz de renderizar
+# imagens através de uma interface que utiliza o método draw. Porém,
+# no momento ela só suporta formato PNG e você também precisa ser capaz
+# de renderizar imagens em SVG. Altere o código, sem modificar a classe
+# SvgImage, para que isso seja possível.
+
+# 😎 Dica: Se você garantir que a imagem SVG seja renderizada utilizando
+# a mesma interface que a imagem PNG, a imagem se tornará compatível.
+
+from abc import ABC, abstractmethod
+
+
+class PngInterface(ABC):
+    @abstractmethod
+    def draw(self):
+        raise NotImplementedError
+
+
+class PngImage(PngInterface):
+    def __init__(self, png_path):
+        self.png_path = png_path
+        self.format = "raster"
+
+    def draw(self):
+        print(f"Drawing PNG {self.png_path} with {self.format}")
+
+
+class SvgImage:
+    def __init__(self, svg_path):
+        self.svg_path = svg_path
+        self.format = "vector"
+
+    def get_image(self):
+        return f"SVG {self.svg_path} with {self.format}"
