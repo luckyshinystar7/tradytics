e483e9a1eb318042ad8b0cb544a9e7ef39bbb6b2
luckyshinystar7 <luckyshinystar7@gmail.com>
2023-02-13 20:58:53 -0300

    inicia exercicio 2

---
+++ 4-ciencia-da-computacao/bloco-35-algoritmos/dia-1-complexidade-de-algoritmos/exercicio_02.py
@@ -0,0 +1,15 @@
+# 🚀 Exercício 2 Suponha que se está escrevendo uma função para um jogo de
+# batalha naval. Dado um array bidimensional com n linhas e m colunas, e
+# um par de coordenadas x para linhas e y para colunas, o algoritmo verifica
+# se há um navio na coordenada alvo. Por exemplo:
+
+# Copiar
+# entrada = [ 0 0 0 0 1
+#             0 0 0 0 1
+#             1 1 1 1 1
+#             0 0 0 1 0 ]
+
+# resultado para (0, 4) = True
+# resultado para (1, 1) = False
+# Qual seria a ordem de complexidade da solução para este problema?
+# E a complexidade de espaço?
