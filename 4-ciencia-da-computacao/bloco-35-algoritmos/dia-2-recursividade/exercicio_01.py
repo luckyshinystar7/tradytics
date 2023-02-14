dc5290ba618ddd3064a76da22ef7246efc212e97
luckyshinystar7 <luckyshinystar7@gmail.com>
2023-02-14 20:33:01 -0300

    finaliza exercicio 1

---
+++ 4-ciencia-da-computacao/bloco-35-algoritmos/dia-2-recursividade/exercicio_01.py
@@ -0,0 +1,8 @@
+# 🚀 Exercício 1: Crie um algoritmo não recursivo para contar quantos números
+# pares existem em uma sequência numérica (1 a n).
+
+def even_elements(n):
+    return (n//2)
+
+
+print(even_elements(7))
