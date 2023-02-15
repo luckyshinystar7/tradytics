a9a6916b88776c51139c1800e91da14973216439
luckyshinystar7 <luckyshinystar7@gmail.com>
2023-02-15 20:55:52 -0300

    finaliza exercicio 1

---
+++ 4-ciencia-da-computacao/bloco-35-algoritmos/dia-3-algoritmos-de-ordenacao-e-busca/exercicio_01.py
@@ -0,0 +1,6 @@
+# Exercício 1 Dado um array com os seguintes elementos ["zebra", "macaco",
+# "elefante", "arara", "javali"], após duas iterações utilizando bubble sort,
+# como estaria este array?
+
+# Ficaria ["elefante", "arara", "javali", "macaco", "zebra"]
+# pois os elementos zebra e macaco seriam levados para o fundo
